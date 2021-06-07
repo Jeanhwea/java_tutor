8c27efa894f650d596cd8549053355e6553b004c
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-07 16:09:30 +0800

    update

---
+++ src/main/java/io/github/jeanhwea/scratch/Solution08.java
@@ -0,0 +1,11 @@
+package io.github.jeanhwea.scratch;
+
+import java.util.*;
+
+public class Solution08 {
+
+  public static void main(String args[]) {
+    System.out.println("hello world");
+  }
+
+}
