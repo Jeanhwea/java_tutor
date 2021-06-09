924f20e313089c196cc8ae51975bea48c0dfbde7
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-09 09:02:14 +0800

    add: 添加有效异位词

--- src/main/java/io/github/jeanhwea/leetcode/Solution.java
+++ src/main/java/io/github/jeanhwea/leetcode/Solution.java
@@ -4,37 +4,27 @@ import java.util.*;
 
 public class Solution {
 
-  public static void sink(int[] a, int p, int n) {
-    int mi = p;
-    while (true) {
-      int s = 2 * p + 1;
-      while (s < n && a[s] < a[mi]) mi = s;
-      ++s;
-      while (s < n && a[s] < a[mi]) mi = s;
-      if (p == mi) break;
+  public static boolean isAnagram(String s, String t) {
+    int n = s.length(), m = t.length(), total = 26;
+    if (n != m) return false;
 
-      int t = a[mi];
-      a[mi] = a[p];
-      a[p] = t;
-      p = mi;
+    int[] a = new int[total], b = new int[total];
+    for (int i = 0; i < n; i++) {
+      int ai = s.charAt(i) - 'a', bi = t.charAt(i) - 'a';
+      a[ai] = a[ai] + 1;
+      b[bi] = b[bi] + 1;
     }
+
+    for (int i = 0; i < total; i++) {
+      if (a[i] != b[i]) return false;
+    }
+
+    return true;
   }
 
   public static void main(String args[]) {
-    int[] a = genArray(8);
-    display(a);
-    for (int i = a.length / 2 - 1; i >= 0; i--) {
-      sink(a, i, a.length);
-    }
-    display(a);
-
-    int[] b = new int[a.length];
-    for (int i = 0; i < b.length; i++) {
-      b[i] = a[0];
-      a[0] = a[b.length - i - 1];
-      sink(a, 0, b.length - i);
-    }
-    display(b);
+    String s = "nl", t = "cx";
+    System.out.println(isAnagram(s, t));
   }
 
   ////////////////////////////////////////////////////////////////////////////////
