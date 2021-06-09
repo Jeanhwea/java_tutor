556ad8f54ac60155f56d364f76b22b9793940db9
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-09 08:18:53 +0800

    add: 反转字符串

---
+++ src/main/java/io/github/jeanhwea/leetcode/Solution344.java
@@ -0,0 +1,29 @@
+package io.github.jeanhwea.leetcode;
+
+import java.util.*;
+
+/**
+ * 反转字符串
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-09, JDK1.8
+ */
+public class Solution344 {
+
+  public static void reverseString(char[] s) {
+    int n = s.length;
+    for (int i = 0; i < n / 2; i++) {
+      char t = s[i];
+      s[i] = s[n - i - 1];
+      s[n - i - 1] = t;
+    }
+  }
+
+  public static void main(String args[]) {
+    char[] a = {'h', 'e', 'l', 'l', 'o'};
+    System.out.println(a);
+    System.out.println("========================================");
+    reverseString(a);
+    System.out.println(a);
+  }
+}
