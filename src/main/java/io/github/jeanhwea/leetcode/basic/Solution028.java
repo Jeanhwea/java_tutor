2edbc2cc51baefd19f2fa2e903ba0303ae5f5a01
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-09 13:48:21 +0800

    add: 实现 strStr()

--- src/main/java/io/github/jeanhwea/leetcode/basic/Solution028.java
+++ src/main/java/io/github/jeanhwea/leetcode/basic/Solution028.java
@@ -1,8 +1,14 @@
-package io.github.jeanhwea.leetcode;
+package io.github.jeanhwea.leetcode.basic;
 
 import java.util.*;
 
-public class Solution {
+/**
+ * 实现 strStr()
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-09, JDK1.8
+ */
+public class Solution028 {
 
   public static int strStr(String s, String t) {
     if (t.isEmpty()) return 0;
@@ -23,27 +29,9 @@ public class Solution {
   }
 
   public static void main(String args[]) {
-    // pass
     System.out.println(strStr("aabc", ""));
     System.out.println(strStr("aabc", "ab"));
     System.out.println(strStr("aabc", "c"));
     System.out.println(strStr("aabc", "a"));
-  }
-
-  ////////////////////////////////////////////////////////////////////////////////
-  public static int[] genArray(int n) {
-    int[] a = new int[n];
-    for (int i = 0; i < n; i++) {
-      a[i] = (int) (Math.random() * 50);
-    }
-    return a;
-  }
-
-  public static void display(int[] a) {
-    System.out.println(Arrays.toString(a));
-  }
-
-  public static void display(Collection<Integer> a) {
-    System.out.println(Arrays.toString(a.toArray()));
   }
 }
