a6f877c29164a525a7a3dfe1ccf38cdc162f7d0e
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-09 09:49:05 +0800

    add: 整理题解包

--- src/main/java/io/github/jeanhwea/leetcode/basic/Solution349.java
+++ src/main/java/io/github/jeanhwea/leetcode/basic/Solution349.java
@@ -1,62 +1,60 @@
-package io.github.jeanhwea.leetcode;
+package io.github.jeanhwea.leetcode.basic;
 
 import java.util.*;
 
 /**
- * 两个数组交集 II
+ * 两个数组交集
  *
  * @author Jinghui Hu
  * @since 2021-06-08, JDK1.8
  */
-public class Solution350 {
+public class Solution349 {
 
   public static int[] intersect(int[] a, int[] b) {
-    int n = a.length, m = b.length, i = 0, j = 0;
+    int n = a.length, m = b.length;
+
+    Set<Integer> s1 = new HashSet<>(), s2 = new HashSet<>();
+    for (int i = 0; i < n; i++) s1.add(a[i]);
+    for (int i = 0; i < m; i++) if (s1.contains(b[i])) s2.add(b[i]);
+
+    int[] res = new int[s2.size()];
+    int k = 0;
+    for (Integer e : s2) {
+      res[k++] = e;
+    }
+    return res;
+  }
+
+  public static int[] intersect1(int[] a, int[] b) {
+    int n = a.length, m = b.length;
     Arrays.sort(a);
     Arrays.sort(b);
 
-    int[] c = new int[Math.min(n, m)];
-    int ci = 0;
-
+    Set<Integer> c = new HashSet<>();
+    int i = 0, j = 0;
     while (i < n && j < m) {
       if (a[i] < b[j]) i++;
       else if (a[i] > b[j]) j++;
       else {
-        c[ci++] = a[i];
+        c.add(a[i]);
         i++;
         j++;
       }
     }
 
-    return Arrays.copyOfRange(c, 0, ci);
-  }
-
-  public static int[] intersect1(int[] a, int[] b) {
-    int n = a.length, m = b.length;
-    Map<Integer, Integer> mp = new HashMap<>();
-    for (int i = 0; i < n; i++) {
-      int count = mp.getOrDefault(a[i], 0) + 1;
-      mp.put(a[i], count);
+    int[] res = new int[c.size()];
+    int k = 0;
+    for (Integer e : c) {
+      res[k++] = e;
     }
-
-    int[] c = new int[m];
-    int ci = 0;
-    for (int i = 0; i < m; i++) {
-      int count = mp.getOrDefault(b[i], 0);
-      if (count > 0) {
-        c[ci++] = b[i];
-        mp.put(b[i], count - 1);
-      }
-    }
-
-    return Arrays.copyOfRange(c, 0, ci);
+    return res;
   }
 
   public static void main(String args[]) {
-    // int[] a = new int[] {4, 9, 5};
-    // int[] b = new int[] {9, 4, 9, 8, 4};
-    int[] a = new int[] {2, 2};
-    int[] b = new int[] {1, 2, 2, 1};
+    int[] a = new int[] {4, 9, 5};
+    int[] b = new int[] {9, 4, 9, 8, 4};
+    // int[] a = new int[] {1};
+    // int[] b = new int[] {1, 2};
     display(a);
     display(b);
     System.out.println("========================================");
--- src/main/java/io/github/jeanhwea/leetcode/basic/Solution387.java
+++ src/main/java/io/github/jeanhwea/leetcode/basic/Solution387.java
@@ -1,4 +1,4 @@
-package io.github.jeanhwea.leetcode;
+package io.github.jeanhwea.leetcode.basic;
 
 import java.util.*;
 
