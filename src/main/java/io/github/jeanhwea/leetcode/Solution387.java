48533ef4a6716ef4867b9591614e39c8beaafc0e
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-09 08:25:26 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/Solution387.java
@@ -0,0 +1,38 @@
+package io.github.jeanhwea.leetcode;
+
+import java.util.*;
+
+/**
+ * 字符串中的第一个唯一字符
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-09, JDK1.8
+ */
+public class Solution387 {
+
+  public static int firstUniqChar(String s) {
+    int n = s.length();
+    Map<Character, Integer> mp = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      char ch = s.charAt(i);
+      int v = mp.getOrDefault(ch, 0) + 1;
+      mp.put(ch, v);
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (1 == mp.get(s.charAt(i))) return i;
+    }
+
+    return -1;
+  }
+
+  public static void main(String args[]) {
+    String s = "loveleetcode";
+    System.out.println(firstUniqChar(s));
+
+    System.out.println("");
+
+    String s1 = "leetcode";
+    System.out.println(firstUniqChar(s1));
+  }
+}
