b4e49904075576f650bb93e1ffd10331613d4e92
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-10 20:35:44 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/Solution.java
+++ src/main/java/io/github/jeanhwea/leetcode/Solution.java
@@ -4,30 +4,32 @@ import java.util.*;
 
 public class Solution {
 
-  public static int strStr(String s, String t) {
-    if (t.isEmpty()) return 0;
+  public static ListNode reverseRange(ListNode head, int m, int n) {
+    ListNode p = head, q = head;
+    for (int i = 0; i < n - 1; i++) {
+      if (i < m - 2) p = p.next;
+      q = q.next;
+    }
+    // System.out.println(q.val);
 
-    int n = s.length(), m = t.length();
-    for (int i = 0; i < n - m + 1; i++) {
-      boolean found = true;
-      for (int j = 0; j < m; j++) {
-        if (s.charAt(i + j) != t.charAt(j)) {
-          found = false;
-          break;
-        }
-      }
-      if (found) return i;
+    ListNode t = p;
+    while (t != q) {
+      t = p.next;
+      p.next = t.next;
+      t.next = p.next;
+      p.next = t;
+      p = t;
     }
 
-    return -1;
+    return head;
   }
 
   public static void main(String args[]) {
-    // pass
-    System.out.println(strStr("aabc", ""));
-    System.out.println(strStr("aabc", "ab"));
-    System.out.println(strStr("aabc", "c"));
-    System.out.println(strStr("aabc", "a"));
+    int[] a = {1, 2, 3, 4, 5};
+    ListNode p = makeList(a);
+    display(p);
+    reverseRange(p, 2, 4);
+    display(p);
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -45,5 +47,44 @@ public class Solution {
 
   public static void display(Collection<Integer> a) {
     System.out.println(Arrays.toString(a.toArray()));
+  }
+
+  public static void display(ListNode head) {
+    List<Integer> vals = new LinkedList<Integer>();
+    ListNode p = head;
+    while (p != null) {
+      vals.add(p.val);
+      p = p.next;
+    }
+    display(vals);
+  }
+
+  public static ListNode makeList(int[] a) {
+    ListNode head = null, p = null;
+    for (int i = 0; i < a.length; i++) {
+      ListNode t = new ListNode(a[i], null);
+      if (head == null) {
+        head = p = t;
+      } else {
+        p = p.next = t;
+      }
+    }
+    return head;
+  }
+
+  public static class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
   }
 }
