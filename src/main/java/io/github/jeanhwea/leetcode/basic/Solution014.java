9bc0d295f01768426e745d34e476da0e0cc9d70b
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-10 18:47:55 +0800

    update

--- src/main/java/io/github/jeanhwea/leetcode/basic/Solution014.java
+++ src/main/java/io/github/jeanhwea/leetcode/basic/Solution014.java
@@ -14,9 +14,9 @@ public class Solution014 {
     int n = strs[0].length();
 
     for (int i = 0; i < n; i++) {
-      char ch1 = strs[0].charAt(i);
+      char ch = strs[0].charAt(i);
       for (int j = 1; j < strs.length; j++) {
-        if (i < strs[j].length() && strs[j].charAt(i) != strs[0].charAt(i)) {
+        if (i < strs[j].length() && strs[j].charAt(i) != ch) {
           return strs[0].substring(0, i);
         }
       }
