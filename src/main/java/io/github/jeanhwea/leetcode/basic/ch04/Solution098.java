fa068797a9492062e915bb8eff693ec47ad7e986
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-12 01:49:10 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/basic/ch04/Solution098.java
+++ src/main/java/io/github/jeanhwea/leetcode/basic/ch04/Solution098.java
@@ -17,12 +17,37 @@ public class Solution098 {
     return trans(root.right, list);
   }
 
-  public static boolean isValidBST(TreeNode root) {
+  public static boolean isValidBST0(TreeNode root) {
     if (root == null) return true;
     List<Integer> list = new LinkedList<>();
     List<Integer> res = trans(root, list);
     for (int i = 0; i < res.size() - 1; i++) {
       if (res.get(i) >= res.get(i + 1)) return false;
+    }
+
+    return true;
+  }
+
+  public static boolean isValidBST(TreeNode root) {
+    Stack<TreeNode> stack = new Stack<>();
+    Integer mi = null;
+
+    while (!stack.isEmpty() || root != null) {
+      while (root != null) {
+        stack.push(root);
+        root = root.left;
+      }
+
+      root = stack.pop();
+      // System.out.println(root.val);
+      if (mi == null) {
+        mi = root.val;
+      } else {
+        if (root.val <= mi) return false;
+        mi = root.val;
+      }
+
+      root = root.right;
     }
 
     return true;
