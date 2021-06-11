0e0547c7478e7cdad9aa9d2a30fc6740cfb6d451
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-11 15:02:36 +0800

    add: 添加删除链表中的节点

---
+++ src/main/java/io/github/jeanhwea/leetcode/basic/ch03/Solution237.java
@@ -0,0 +1,25 @@
+package io.github.jeanhwea.leetcode.basic.ch03;
+
+import java.util.*;
+
+/**
+ * 删除链表中的节点
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-11, JDK1.8
+ */
+public class Solution237 {
+
+  public static void deleteNode(ListNode node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+  }
+
+  public static void main(String args[]) {
+    ListNode head = ListNode.makeList(new int[] {1, 2, 3, 4, 5});
+    ListNode node = head.next;
+    ListNode.display(head);
+    deleteNode(node);
+    ListNode.display(head);
+  }
+}
