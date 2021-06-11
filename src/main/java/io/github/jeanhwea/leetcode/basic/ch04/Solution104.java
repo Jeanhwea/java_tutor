c903d87e7adcd722e1aca0d7194f7992b2902c8e
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-12 00:47:54 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/basic/ch04/Solution104.java
+++ src/main/java/io/github/jeanhwea/leetcode/basic/ch04/Solution104.java
@@ -10,7 +10,27 @@ import java.util.*;
  */
 public class Solution104 {
 
+  // 层次遍历
   public static int maxDepth(TreeNode root) {
+    if (root == null) return 0;
+
+    int depth = 0;
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      depth++;
+      int n = queue.size();
+      for (int i = 0; i < n; i++) {
+        TreeNode p = queue.poll();
+        if (p.left != null) queue.offer(p.left);
+        if (p.right != null) queue.offer(p.right);
+      }
+    }
+    return depth;
+  }
+
+  // 递归版
+  public static int maxDepth1(TreeNode root) {
     if (root == null) return 0;
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
   }
