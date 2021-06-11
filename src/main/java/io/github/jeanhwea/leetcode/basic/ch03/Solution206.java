c36c3e1b8edb80d62b24bc38744b2140b707b301
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-11 13:40:21 +0800

    add: 添加反转链表

--- src/main/java/io/github/jeanhwea/leetcode/basic/ch03/Solution206.java
+++ src/main/java/io/github/jeanhwea/leetcode/basic/ch03/Solution206.java
@@ -10,8 +10,29 @@ import java.util.*;
  */
 public class Solution206 {
 
+  public static ListNode reverseList(ListNode head) {
+    ListNode p = head, q = head;
+
+    while (p != null) {
+      if (p == head) {
+        p = p.next;
+        head.next = null;
+      } else {
+        q = head;
+        head = p;
+        p = p.next;
+        head.next = q;
+      }
+    }
+
+    return head;
+  }
+
   public static void main(String args[]) {
-    ListNode head = ListNode.makeList(new int[] {1, 2, 3});
+    ListNode head = ListNode.makeList(new int[] {1, 2, 3, 4, 5});
+    ListNode.display(head);
+    System.out.println("========================================");
+    head = reverseList(head);
     ListNode.display(head);
   }
 }
