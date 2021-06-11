b7e01fc0f0055df5cb0d4320e262540a1fd1692d
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-11 13:19:52 +0800

    add: 添加统计解法

--- src/main/java/io/github/jeanhwea/leetcode/basic/Solution038.java
+++ src/main/java/io/github/jeanhwea/leetcode/basic/Solution038.java
@@ -10,7 +10,8 @@ import java.util.*;
  */
 public class Solution038 {
 
-  public static String countAndSay(int k) {
+  // 暴露统计解法
+  public static String countAndSay0(int k) {
     if (k <= 1) return "1";
     String p = countAndSay(k - 1), s = "";
     int n = p.length(), count = 0;
@@ -32,9 +33,30 @@ public class Solution038 {
     return s;
   }
 
+  // 双指针解法
+  public static String countAndSay(int k) {
+    if (k <= 1) return "1";
+    String s0 = "1";
+    for (int i = 1; i < k; i++) {
+      int n = s0.length();
+      String s = "";
+      int p = 0, q = 0;
+      while (p < n) {
+        while (p < n && s0.charAt(p) == s0.charAt(q)) p++;
+        s += String.valueOf(p - q) + s0.charAt(q);
+        q = p;
+      }
+      s0 = s;
+    }
+    return s0;
+  }
+
   public static void main(String args[]) {
+    // System.out.println(countAndSay(4));
     for (int i = 0; i < 10; i++) {
+      System.out.println(countAndSay0(i));
       System.out.println(countAndSay(i));
+      System.out.println("=======");
     }
   }
 }
