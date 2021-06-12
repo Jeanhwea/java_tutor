d68e285d9b76b9858466305049bc19a29b2d39af
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-12 14:40:58 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/basic/ch04/TreeNode.java
+++ src/main/java/io/github/jeanhwea/leetcode/basic/ch04/TreeNode.java
@@ -26,35 +26,73 @@ public class TreeNode {
   }
 
   public static TreeNode makeTree(int[] a) {
-    int n = a.length, m = 1;
-    if (n <= 0) return null;
-    TreeNode root = new TreeNode(a[0]);
+    if (a.length < 1) return null;
+    int n = a.length, m = 1, s = 0;
+
     Queue<TreeNode> queue = new LinkedList<>();
+    TreeNode root = new TreeNode(a[0]), p = null;
     queue.offer(root);
     while (!queue.isEmpty()) {
       int size = queue.size();
       for (int i = 0; i < size; i++) {
-        TreeNode p = queue.poll();
-        int k = m + 2 * i;
-        if (k < n && a[k] > 0) {
-          TreeNode q = new TreeNode(a[k]);
-          p.left = q;
-          queue.offer(q);
+        TreeNode node = queue.poll();
+        s = 2 * i + m;
+        if (s < n && a[s] >= 0) {
+          p = new TreeNode(a[s]);
+          node.left = p;
+          queue.offer(p);
         }
-        k++;
-        if (k < n && a[k] > 0) {
-          TreeNode q = new TreeNode(a[k]);
-          p.right = q;
-          queue.offer(q);
+        s++;
+        if (s < n && a[s] >= 0) {
+          p = new TreeNode(a[s]);
+          node.right = p;
+          queue.offer(p);
         }
+        s++;
       }
-      m += 2 * size;
+      m = s;
     }
+
     return root;
   }
 
-  public static void main(String args[]) {
-    TreeNode root = makeTree(new int[] {1, 2, 3, 4, 5});
-    System.out.println("====");
+  public static void display(TreeNode root) {
+    if (root == null) {
+      System.out.println("null");
+    }
+
+    List<Integer> vals = new LinkedList<>();
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    while (true) {
+      int size = queue.size();
+      boolean done = true;
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+        if (null == node) {
+          vals.add(-1);
+          continue;
+        } else {
+          vals.add(node.val);
+        }
+
+        if (null == node.left) {
+          queue.offer(null);
+        } else {
+          queue.offer(node.left);
+          done = false;
+        }
+
+        if (null == node.right) {
+          queue.offer(null);
+        } else {
+          queue.offer(node.right);
+          done = false;
+        }
+      }
+      if (done) break;
+    }
+
+    System.out.println(Arrays.toString(vals.toArray()));
   }
 }
