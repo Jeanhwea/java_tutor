59ddd28921e55acae9b4f7d8247e2f2d486cfa6d
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-13 13:55:23 +0800

    add: 添加爬楼梯的求解

---
+++ src/main/java/io/github/jeanhwea/leetcode/basic/ch06/Solution070.java
@@ -0,0 +1,27 @@
+package io.github.jeanhwea.leetcode.basic.ch06;
+
+import java.util.*;
+
+/**
+ * 爬楼梯
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-13, JDK1.8
+ */
+public class Solution070 {
+
+  public static int climbStairs(int n) {
+    int[] dp = new int[n + 1];
+    dp[0] = dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+  }
+
+  public static void main(String args[]) {
+    for (int i = 1; i < 6; i++) {
+      System.out.println(climbStairs(i));
+    }
+  }
+}
