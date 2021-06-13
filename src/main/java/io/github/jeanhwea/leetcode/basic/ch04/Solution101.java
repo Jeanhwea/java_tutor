0c27acff9bc800a0a378aa61211d4f1341736c9f
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-13 08:48:54 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/basic/ch04/Solution101.java
+++ src/main/java/io/github/jeanhwea/leetcode/basic/ch04/Solution101.java
@@ -13,6 +13,31 @@ public class Solution101 {
   public static boolean isSymmetric(TreeNode root) {
     if (root == null) return true;
 
+    Queue<TreeNode> n1 = new LinkedList<>();
+    Queue<TreeNode> n2 = new LinkedList<>();
+    n1.offer(root);
+    n2.offer(root);
+
+    while (!n1.isEmpty()) {
+      TreeNode p1 = n1.poll(), p2 = n2.poll();
+      if (p1 != null && p2 == null) return false;
+      if (p2 != null && p1 == null) return false;
+      if (p1 != null && p2 != null && p1.val != p2.val) return false;
+
+      if (p1 != null) {
+        n1.offer(p1.left);
+        n2.offer(p2.right);
+        n1.offer(p1.right);
+        n2.offer(p2.left);
+      }
+    }
+
+    return true;
+  }
+
+  public static boolean isSymmetric0(TreeNode root) {
+    if (root == null) return true;
+
     Queue<TreeNode> nodes = new LinkedList<>();
     nodes.add(root);
     while (!nodes.isEmpty()) {
@@ -54,9 +79,19 @@ public class Solution101 {
   }
 
   public static void main(String args[]) {
-    TreeNode tree = TreeNode.makeTree(new int[] {2, 3, 3});
-    TreeNode.display(tree);
+    TreeNode tree1 = TreeNode.makeTree(new int[] {2, 3, 3});
+    TreeNode.display(tree1);
     System.out.println("========================================");
-    System.out.println(isSymmetric(tree));
+    System.out.println(isSymmetric(tree1));
+
+    TreeNode tree2 = TreeNode.makeTree(new int[] {2});
+    TreeNode.display(tree2);
+    System.out.println("========================================");
+    System.out.println(isSymmetric(tree2));
+
+    TreeNode tree3 = TreeNode.makeTree(new int[] {1, 2, 2, 4, -1, 4});
+    TreeNode.display(tree3);
+    System.out.println("========================================");
+    System.out.println(isSymmetric(tree3));
   }
 }
