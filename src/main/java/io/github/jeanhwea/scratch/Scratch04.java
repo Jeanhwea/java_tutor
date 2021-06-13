4736c5b9de9bcf07494254e685b106fd19157d4e
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-13 11:37:09 +0800

    add

--- src/main/java/io/github/jeanhwea/scratch/Scratch04.java
+++ src/main/java/io/github/jeanhwea/scratch/Scratch04.java
@@ -10,68 +10,35 @@ import java.util.*;
  */
 public class Scratch04 {
 
-  public static ListNode reverseRange(ListNode head, int m, int n) {
-    ListNode p = head, q = null, t = null, s = null;
-    for (int i = 0; i < n; i++) {
-      if (i < m - 1) {
-        q = p;
-        p = p != null ? p.next : null;
-      } else if (i < m) {
-        t = p;
-        p = p.next;
-      } else {
-        if (q == null) {
-          s = head;
-          head = p;
-          p = p.next;
-          head.next = s;
-        } else {
-          s = q.next;
-          q.next = p;
-          p = p.next;
-          q.next.next = s;
-        }
-      }
+  ////////////////////////////////////////////////////////////////////////////////
+  // 插入链表
+  public static ListNode insert(ListNode head, int k, int val) {
+    ListNode p = null;
+    for (int i = 0; i < k - 1; i++) {
+      p = p != null ? p.next : head;
     }
 
-    if (t != null) t.next = p;
+    ListNode q = new ListNode(val);
+    if (p == null) {
+      q.next = head;
+      head = q;
+    } else {
+      q.next = p.next;
+      p.next = q;
+    }
 
     return head;
   }
 
   ////////////////////////////////////////////////////////////////////////////////
-  // 反转链表
-  public static ListNode reverse(ListNode head) {
-    ListNode p = head, q;
-
-    while (p != null) {
-      if (p == head) {
-        p = p.next;
-        head.next = null;
-      } else {
-        q = head;
-        head = p;
-        p = p.next;
-        head.next = q;
-      }
+  // 删除链表
+  public static ListNode remove(ListNode head, int k) {
+    ListNode p = null;
+    for (int i = 0; i < k - 1; i++) {
+      p = p != null ? p.next : head;
     }
 
-    return head;
-  }
-
-  public static ListNode removeNthFromEnd(ListNode head, int k) {
-    int n = 0;
-    ListNode p = head;
-    while (p != null) {
-      n++;
-      p = p.next;
-    }
-    // System.out.println(n);
-
-    p = head;
-    for (int i = 0; i < n - k - 1; i++) p = p.next;
-
-    if (k == n) {
+    if (p == null) {
       head = head.next;
     } else {
       p.next = p.next.next;
@@ -80,26 +47,16 @@ public class Scratch04 {
     return head;
   }
 
-  public static void main(String args[]) {
-    ListNode head = ListNode.makeList(new int[] {1, 2, 3, 4});
-    ListNode.display(head);
-    ListNode.display(removeNthFromEnd(head, 1));
-  }
-
   ////////////////////////////////////////////////////////////////////////////////
-  public static int[] genArray(int n) {
-    int[] a = new int[n];
-    for (int i = 0; i < n; i++) {
-      a[i] = (int) (Math.random() * 50);
-    }
-    return a;
+  // 反转链表
+  public static ListNode reverse(ListNode head) {
+    return null;
   }
 
-  public static void display(int[] a) {
-    System.out.println(Arrays.toString(a));
-  }
-
-  public static void display(Collection<Integer> a) {
-    System.out.println(Arrays.toString(a.toArray()));
+  public static void main(String args[]) {
+    ListNode head = ListNode.makeList(new int[] {1, 2, 3, 4, 5, 6, 7});
+    ListNode.display(head);
+    System.out.println("========================================");
+    ListNode.display(remove(head, 7));
   }
 }
