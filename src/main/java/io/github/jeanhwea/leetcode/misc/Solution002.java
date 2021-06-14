7a3d2b68b19091c4f9c4680faadedf01767a2994
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-14 12:57:52 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/misc/Solution002.java
+++ src/main/java/io/github/jeanhwea/leetcode/misc/Solution002.java
@@ -35,56 +35,12 @@ public class Solution002 {
   }
 
   public static void main(String args[]) {
-    ListNode l1 = makeList(new int[] {2, 3, 4});
-    ListNode l2 = makeList(new int[] {5, 6, 4});
+    ListNode l1 = ListNode.makeList(new int[] {2, 3, 4});
+    ListNode l2 = ListNode.makeList(new int[] {5, 6, 4});
     ListNode ans = addTwoNumbers(l1, l2);
-    System.out.println(listToString(l1));
-    System.out.println(listToString(l2));
+    ListNode.display(l1);
+    ListNode.display(l2);
     System.out.println("========================================");
-    System.out.println(listToString(ans));
-  }
-
-  public static ListNode makeList(int[] a) {
-    ListNode h = null, t = null;
-    for (int i = 0; i < a.length; i++) {
-      if (h == null) {
-        h = new ListNode(a[i], null);
-        t = h;
-      } else {
-        t.next = new ListNode(a[i], null);
-        t = t.next;
-      }
-    }
-    return h;
-  }
-
-  public static String listToString(ListNode h) {
-    ListNode t = h;
-    if (t == null) {
-      return "[]";
-    }
-
-    String result = "";
-    while (t != null) {
-      result += Integer.toString(t.val) + ", ";
-      t = t.next;
-    }
-    return "[" + result.substring(0, result.length() - 2) + "]";
-  }
-
-  public static class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {}
-
-    ListNode(int val) {
-      this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-      this.val = val;
-      this.next = next;
-    }
+    ListNode.display(ans);
   }
 }
