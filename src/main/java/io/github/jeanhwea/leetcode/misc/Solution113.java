c5a712cc231aa53284f898892657a192a9876896
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-14 11:45:05 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/misc/Solution113.java
@@ -0,0 +1,57 @@
+package io.github.jeanhwea.leetcode.misc;
+
+import java.util.*;
+
+/**
+ * 路径求和 II
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-14, JDK1.8
+ */
+public class Solution113 {
+  public static List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+    List<List<Integer>> ans = new LinkedList<>();
+
+    if (root == null) return ans;
+
+    Stack<TreeNode> nodes = new Stack<>();
+    Map<TreeNode, TreeNode> parent = new HashMap<>();
+    Map<TreeNode, Integer> sum = new HashMap<>();
+    nodes.push(root);
+    parent.put(root, null);
+    sum.put(root, root.val);
+    while (!nodes.isEmpty()) {
+      TreeNode p = nodes.pop();
+      if (p.left == null && p.right == null && sum.get(p) == targetSum) {
+        List<Integer> path = new LinkedList<>();
+        TreeNode q = p;
+        while (q != null) {
+          path.add(q.val);
+          q = parent.get(q);
+        }
+        Collections.reverse(path);
+        ans.add(path);
+      }
+
+      int s0 = sum.getOrDefault(p, 0);
+      if (p.left != null) {
+        nodes.add(p.left);
+        sum.put(p.left, s0 + p.left.val);
+        parent.put(p.left, p);
+      }
+      if (p.right != null) {
+        nodes.add(p.right);
+        sum.put(p.right, s0 + p.right.val);
+        parent.put(p.right, p);
+      }
+    }
+
+    return ans;
+  }
+
+  public static void main(String args[]) {
+    TreeNode tree1 = TreeNode.makeTree(new int[] {5, 4, 8, 11, -1, 13, 4, 7, 2, -1, -1, 5, 1});
+    int targetSum = 22;
+    TreeNode.display(tree1);
+  }
+}
---
+++ src/main/java/io/github/jeanhwea/leetcode/misc/TreeNode.java
@@ -0,0 +1,140 @@
+package io.github.jeanhwea.leetcode.misc;
+
+import java.util.*;
+
+/**
+ * 二叉树节点
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-11, JDK1.8
+ */
+public class TreeNode {
+
+  int val;
+  TreeNode left, right;
+
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+
+  public static TreeNode makeTree(int[] a) {
+    int n = a.length;
+    if (a.length < 1) return null;
+
+    Queue<TreeNode> nodes = new LinkedList<>();
+    TreeNode root = new TreeNode(a[0]), p = null;
+    nodes.offer(root);
+
+    int depth = 1;
+    while (true) {
+      int size = 1 << depth;
+      if (n < size) break;
+
+      for (int i = 0; i < size / 2; i++) {
+        p = nodes.poll();
+        if (p == null) {
+          nodes.offer(null);
+          nodes.offer(null);
+        } else {
+          int s = 2 * i + size - 1;
+          if (s < n) {
+            if (a[s] >= 0) {
+              p.left = new TreeNode(a[s]);
+              nodes.offer(p.left);
+            } else {
+              nodes.offer(null);
+            }
+          } else break;
+
+          s++;
+          if (s < n) {
+            if (a[s] >= 0) {
+              p.right = new TreeNode(a[s]);
+              nodes.offer(p.right);
+            } else {
+              nodes.offer(null);
+            }
+          } else break;
+        }
+      }
+
+      depth++;
+    }
+
+    return root;
+  }
+
+  public static void display(TreeNode root) {
+    if (root == null) {
+      System.out.println("null");
+      return;
+    }
+
+    List<Integer> values = new ArrayList<>();
+    Queue<TreeNode> nodes = new LinkedList<>();
+    nodes.offer(root);
+    values.add(root.val);
+
+    int depth = 1;
+    while (true) {
+      int size = 1 << depth;
+      boolean done = true;
+      for (int i = 0; i < size / 2; i++) {
+        TreeNode p = nodes.poll();
+        if (p == null) {
+          nodes.offer(null);
+          nodes.offer(null);
+          values.add(-1);
+          values.add(-1);
+        } else {
+          if (p.left != null) {
+            nodes.offer(p.left);
+            values.add(p.left.val);
+            done = false;
+          } else {
+            nodes.offer(null);
+            values.add(-1);
+          }
+
+          if (p.right != null) {
+            nodes.offer(p.right);
+            values.add(p.right.val);
+            done = false;
+          } else {
+            nodes.offer(null);
+            values.add(-1);
+          }
+        }
+      }
+      if (done) break;
+      depth++;
+    }
+
+    int k = values.size() - 1;
+    while (k >= 0) {
+      if (values.get(k) >= 0) break;
+      k--;
+    }
+    values = values.subList(0, k + 1);
+
+    System.out.println(Arrays.toString(values.toArray()));
+  }
+
+  public static void main(String args[]) {
+    // TreeNode root = makeTree(new int[] {1, 2, 3, 4, 5});
+    TreeNode tree4 = makeTree(new int[] {1, 2, 3, -1, 4, 5, 6, -1, -1, 7});
+    // TreeNode tree4 = makeTree(new int[] {1, 2, 3});
+    display(tree4);
+    // System.out.println(1 << 3);
+    // display(root);
+    // System.out.println("====");
+  }
+}
