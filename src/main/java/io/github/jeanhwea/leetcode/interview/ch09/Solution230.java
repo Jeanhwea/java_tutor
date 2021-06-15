c213c2a074d3dab19effd492e96bc68bc8160d6b
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-15 15:23:52 +0800

    add: 添加二叉搜索树第K小的元素

---
+++ src/main/java/io/github/jeanhwea/leetcode/interview/ch09/Solution230.java
@@ -0,0 +1,47 @@
+package io.github.jeanhwea.leetcode.interview.ch09;
+
+import java.util.*;
+
+/**
+ * 二叉搜索树中第K小的元素
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-15, JDK1.8
+ */
+public class Solution230 {
+
+  public static int kthSmallest(TreeNode root, int k) {
+    if (root == null) return -1;
+
+    Deque<TreeNode> nodes = new LinkedList<>();
+    int count = 0;
+    while (root != null || !nodes.isEmpty()) {
+      while (root != null) {
+        nodes.push(root);
+        root = root.left;
+      }
+
+      root = nodes.pop();
+      // System.out.printf("[%d]=%d\n", count, root.val);
+
+      if (++count >= k) {
+        return root.val;
+      }
+
+      if (root.right != null) {
+        root = root.right;
+      } else {
+        root = null;
+      }
+    }
+
+    return -1;
+  }
+
+  public static void main(String args[]) {
+    TreeNode tree1 = TreeNode.makeTree(new int[] {3, 1, 4, -1, 2});
+    TreeNode.display(tree1);
+    System.out.println("========================================");
+    System.out.println(kthSmallest(tree1, 4));
+  }
+}
