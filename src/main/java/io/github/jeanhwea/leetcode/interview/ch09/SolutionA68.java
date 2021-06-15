4373cfdaebb88a56c93cf096cdd5b1a22a6a87d1
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-15 13:15:14 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/interview/ch09/SolutionA68.java
@@ -0,0 +1,21 @@
+package io.github.jeanhwea.leetcode.interview.ch09;
+
+import java.util.*;
+
+/**
+ * 二叉树的最近公共祖先
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-15, JDK1.8
+ */
+public class SolutionA68 {
+
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    // pass
+    return null;
+  }
+
+  public static void main(String args[]) {
+    TreeNode tree1 = TreeNode.makeTree(new int[] {3, 5, 1, 6, 2, 0, 8, -1, -1, 7, 4});
+  }
+}
