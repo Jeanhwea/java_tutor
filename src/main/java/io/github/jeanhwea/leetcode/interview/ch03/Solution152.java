901d65eefad7376f6050a78d2697708ca6418f87
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-15 11:27:44 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/interview/ch03/Solution152.java
+++ src/main/java/io/github/jeanhwea/leetcode/interview/ch03/Solution152.java
@@ -26,14 +26,28 @@ public class Solution152 {
   // 动态规划
   public static int maxProduct(int[] a) {
     int n = a.length;
-    return 0;
+    int[][] dp = new int[n][2];
+    dp[0][0] = dp[0][1] = a[0];
+    for (int i = 1; i < n; i++) {
+      dp[i][0] = Math.max(Math.max(a[i], dp[i - 1][0] * a[i]), dp[i - 1][1] * a[i]);
+      dp[i][1] = Math.min(Math.min(a[i], dp[i - 1][0] * a[i]), dp[i - 1][1] * a[i]);
+    }
+
+    // System.out.println(Arrays.deepToString(dp).replace("],", "],\n"));
+
+    int max = dp[0][0];
+    for (int i = 0; i < n; i++) {
+      if (dp[i][0] > max) max = dp[i][0];
+    }
+    return max;
   }
 
   public static void main(String args[]) {
-    // int[] nums = {4, -9, -1, 2, 3, -1, 4};
-    int[] nums = {0, 2};
+    int[] nums = {4, -9, -1, 2, 3, -1, 4};
+    // int[] nums = {0, 2};
     System.out.println(Arrays.toString(nums));
     System.out.println("========================================");
     System.out.println(maxProduct0(nums));
+    System.out.println(maxProduct(nums));
   }
 }
