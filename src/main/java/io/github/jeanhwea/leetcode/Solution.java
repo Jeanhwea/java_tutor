6e3be0bb01365395ff7e5bd3f8a7a703c3bb7918
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-15 09:43:48 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/Solution.java
+++ src/main/java/io/github/jeanhwea/leetcode/Solution.java
@@ -4,23 +4,24 @@ import java.util.*;
 
 public class Solution {
 
-  public static List<String> fizzBuzz(int n) {
-    List<String> ans = new LinkedList<String>();
-    for (int i = 1; i <= n; i++) {
-      if (i%3==0 && i%5==0) {
-        ans.add("FizzBuzz");
-      } else if (i%3==0) {
-        ans.add("Fizz");
-      } else if (i%5==0) {
-        ans.add("Buzz");
-      } else {
-        ans.add(String.valueOf(i));
-      }
+  public static List<String> tokenize(String s, List<String> dict) {
+    List<List<String>> sentences = new LinkedList<>();
+    Set<String> validWord = new HashSet<>(dict);
+
+    List<String> ans = new LinkedList<>();
+    for (int i = 0; i < sentences.size(); i++) {
+      String str = String.join(" ", sentences.get(i));
+      ans.add(str);
     }
     return ans;
   }
 
   public static void main(String args[]) {
-    System.out.println(Arrays.toString(fizzBuzz(15).toArray()));
+    String s = "abcb";
+    List<String> dict = Arrays.asList("a", "bc", "ac");
+    System.out.println(s);
+    System.out.println(dict);
+    System.out.println("========================================");
+    tokenize(s, dict);
   }
 }
