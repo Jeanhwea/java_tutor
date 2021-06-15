28ac262950ab473f103ceff6630d9838e0d065b1
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-15 10:12:32 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/Solution.java
+++ src/main/java/io/github/jeanhwea/leetcode/Solution.java
@@ -4,9 +4,12 @@ import java.util.*;
 
 public class Solution {
 
-  public static List<String> tokenize(String s, List<String> dict) {
+  public static List<String> tokenize(String s, List<String> wordDict) {
+    Set<String> dict = new HashSet<>(wordDict);
+
     List<List<String>> sentences = new LinkedList<>();
-    Set<String> validWord = new HashSet<>(dict);
+    List<String> choose = new LinkedList<>();
+    backtrack(s, 0, choose, sentences, dict);
 
     List<String> ans = new LinkedList<>();
     for (int i = 0; i < sentences.size(); i++) {
@@ -16,12 +19,31 @@ public class Solution {
     return ans;
   }
 
+  public static void backtrack(
+      String s, int k, List<String> choose, List<List<String>> sentences, Set<String> dict) {
+    int n = s.length();
+    if (k == n) {
+      sentences.add(new ArrayList(choose));
+      return;
+    }
+
+    for (int i = k; i < n; i++) {
+      String word = s.substring(k, i + 1);
+      if (dict.contains(word)) {
+        choose.add(s.substring(k, i + 1));
+        backtrack(s, i + 1, choose, sentences, dict);
+        choose.remove(choose.size() - 1);
+      }
+    }
+  }
+
   public static void main(String args[]) {
     String s = "abcb";
-    List<String> dict = Arrays.asList("a", "bc", "ac");
+    List<String> wordDict = Arrays.asList("a", "bc", "ac", "b");
     System.out.println(s);
-    System.out.println(dict);
+    System.out.println(wordDict);
     System.out.println("========================================");
-    tokenize(s, dict);
+    List<String> ans = tokenize(s, wordDict);
+    System.out.println(ans);
   }
 }
