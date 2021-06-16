5f001c4fb4faa104687f4e2be7f1f11055be198f
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-16 09:49:11 +0800

    add: 添加前缀树模板 2

--- src/main/java/io/github/jeanhwea/leetcode/interview/ch04/Solution208.java
+++ src/main/java/io/github/jeanhwea/leetcode/interview/ch04/Solution208.java
@@ -11,30 +11,59 @@ public class Solution208 {
   public static class Trie {
 
     private boolean isLeaf;
-    private char[] children;
+    private Trie[] children;
 
     public Trie() {
-      this.isLeaf = true;
-      this.children = new char[26];
+      this.isLeaf = false;
+      this.children = new Trie[26];
     }
 
     public void insert(String word) {
+      Trie p = this;
       int n = word.length();
       for (int i = 0; i < n; i++) {
         char ch = word.charAt(i);
+        if (p.children[ch - 'a'] == null) {
+          p.children[ch - 'a'] = new Trie();
+        }
+        p = p.children[ch - 'a'];
       }
+      p.isLeaf = true;
     }
 
     public boolean search(String word) {
-      return false;
+      Trie p = this;
+      int n = word.length(), i = 0;
+      while (i < n) {
+        char ch = word.charAt(i++);
+        p = p.children[ch - 'a'];
+        if (p == null) break;
+      }
+      return p != null && p.isLeaf && (i == n);
     }
 
     public boolean startsWith(String prefix) {
-      return false;
+      Trie p = this;
+      int n = prefix.length(), i = 0;
+      while (i < n) {
+        char ch = prefix.charAt(i++);
+        p = p.children[ch - 'a'];
+        if (p == null) break;
+      }
+
+      return (i == n);
     }
   }
 
   public static void main(String args[]) {
     Trie trie1 = new Trie();
+    trie1.search("a");
+    // trie1.insert("ab");
+    // System.out.println(trie1.search("ab"));
+    // System.out.println(trie1.search("a"));
+    // System.out.println(trie1.startsWith("a"));
+
+    // trie1.insert("a");
+    // System.out.println(trie1.search("a"));
   }
 }
