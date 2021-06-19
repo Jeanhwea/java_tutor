e64f47e014a3d1a4904ffbbfbb0f2728d3e2667d
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-19 14:18:47 +0800

    add: 扁平化嵌套列表迭代器

---
+++ src/main/java/io/github/jeanhwea/leetcode/interview/ch06/NestedInteger.java
@@ -0,0 +1,17 @@
+package io.github.jeanhwea.leetcode.interview.ch06;
+
+import java.util.*;
+
+public class NestedInteger {
+  public boolean isInteger() {
+    return true;
+  }
+
+  public Integer getInteger() {
+    return 0;
+  }
+
+  public List<NestedInteger> getList() {
+    return new LinkedList<>();
+  }
+}
