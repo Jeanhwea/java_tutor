dc11d949edd72b56285c1d364c4511a93d941c19
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-20 05:06:45 +0800

    add: 搜索旋转数组

---
+++ src/main/java/io/github/jeanhwea/leetcode/soulmachine/ch02/Solution033.java
@@ -0,0 +1,41 @@
+package io.github.jeanhwea.leetcode.soulmachine.ch02;
+
+import java.util.*;
+
+/**
+ * 搜索旋转排序数组
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-20, JDK1.8
+ */
+public class Solution033 {
+
+  public static int search(int[] a, int k) {
+    int n = a.length, lo = 0, hi = n - 1, mid;
+    while (lo <= hi) {
+      mid = lo + (hi - lo) / 2;
+      // System.out.printf("lo=%d,hi=%d\n", lo, hi);
+      if (a[mid] == k) return mid;
+
+      if (a[lo] <= a[mid]) {
+        if (a[lo] <= k && k < a[mid]) {
+          hi = mid;
+        } else {
+          lo = mid + 1;
+        }
+      } else {
+        if (a[mid] < k && k <= a[hi]) {
+          lo = mid + 1;
+        } else {
+          hi = mid;
+        }
+      }
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    int[] a = {5, 1, 3};
+    System.out.println(search(a, 3));
+  }
+}
