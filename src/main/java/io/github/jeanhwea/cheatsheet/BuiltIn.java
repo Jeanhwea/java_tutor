94a9cb97ba0bcfd93d52d8e2da11c5471a61a1ce
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-20 19:13:46 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/cheatsheet/BuiltIn.java
@@ -0,0 +1,44 @@
+package io.github.jeanhwea.cheatsheet;
+
+import java.util.*;
+
+/**
+ * @author Jinghui Hu
+ * @since 2021-06-20, JDK1.8
+ */
+public class BuiltIn {
+
+  public static void array() {
+    // 定义数组
+    int[] arr1 = new int[10];
+    int[] arr2 = {1, 2, 3, 4};
+    System.out.println(Arrays.toString(arr2));
+
+    Arrays.fill(arr1, 1); // 填充数组
+    System.out.println(Arrays.toString(arr1));
+
+    // 数组复制
+    int[] arr3 = Arrays.copyOf(arr2, arr2.length);
+    int[] arr4 = Arrays.copyOfRange(arr2, 2, 3); // => [3]
+    System.out.println(Arrays.toString(arr4));
+
+    // 变长数组
+    List<Integer> vector1 = new ArrayList<Integer>();
+    vector1.add(1); // 添加元素到数组最后
+    vector1.add(3);
+    vector1.get(0); // 获取第 0 个元素
+    vector1.size(); // 数组的长度
+    System.out.println(vector1);
+    vector1.add(0, 8); // 添加到数组的第 0 个元素
+    System.out.println(vector1);
+    vector1.set(1, 4); // 修改数组的第 1 个元素
+    System.out.println(vector1);
+    vector1.remove(1); // 删除第 1 个元素
+    System.out.println(vector1);
+    // System.out.println(vector2);
+  }
+
+  public static void main(String[] args) {
+    array();
+  }
+}
---
+++ src/main/java/io/github/jeanhwea/leetcode/interview/ch11/Solution395.java
@@ -0,0 +1,21 @@
+package io.github.jeanhwea.leetcode.interview.ch11;
+
+import java.util.*;
+
+/**
+ * 至少有 K 个重复字符的最长子串
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-20, JDK1.8
+ */
+public class Solution395 {
+
+  public static int longestSubstring(String s, int k) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    String s = "ababbc", k = 2;
+    System.out.println(longestSubstring(s, k));
+  }
+}
