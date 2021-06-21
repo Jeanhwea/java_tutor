0de940cd3cc434b86a7d6b13151a56ee6e867471
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-21 20:10:51 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/concurrent/Runner01.java
@@ -0,0 +1,77 @@
+package io.github.jeanhwea.concurrent;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Runner01 {
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // 不加锁（不安全操作）
+  public static Integer num1 = 0;
+
+  public static void test01() {
+    for (int i = 0; i < 10; i++) {
+      Thread t =
+          new Thread(
+              new Runnable() {
+                @Override
+                public void run() {
+                  while (num1 < 999) {
+                    String threadName = Thread.currentThread().getName();
+                    System.out.printf("%03d: %s\n", num1++, threadName);
+                  }
+                }
+              });
+      t.start();
+    }
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // 悲观锁
+  public static Integer num2 = 0;
+
+  public static void test02() {
+    for (int i = 0; i < 10; i++) {
+      Thread t =
+          new Thread(
+              new Runnable() {
+                @Override
+                public void run() {
+                  synchronized (Runner01.class) {
+                    while (num1 < 999) {
+                      String threadName = Thread.currentThread().getName();
+                      System.out.printf("%03d: %s\n", num1++, threadName);
+                    }
+                  }
+                }
+              });
+      t.start();
+    }
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // CAS 乐观锁
+  public static AtomicInteger num3 = new AtomicInteger(0);
+
+  public static void test03() {
+    for (int i = 0; i < 10; i++) {
+      Thread t =
+          new Thread(
+              new Runnable() {
+                @Override
+                public void run() {
+                  while (num3.get() < 999) {
+                    String threadName = Thread.currentThread().getName();
+                    System.out.printf("%03d: %s\n", num3.getAndIncrement(), threadName);
+                  }
+                }
+              });
+      t.start();
+    }
+  }
+
+  public static void main(String[] args) {
+    test01();
+    // test02();
+    // test03();
+  }
+}
