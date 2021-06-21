1c2ca53ca62310b19ae68030a14b6ec2fb5b164a
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-21 14:12:31 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/interview/ch11/Solution395.java
+++ src/main/java/io/github/jeanhwea/leetcode/interview/ch11/Solution395.java
@@ -11,7 +11,34 @@ import java.util.*;
 public class Solution395 {
 
   public static int longestSubstring(String s, int k) {
-    return 0;
+    return dfs(s, 0, s.length() - 1, k);
+  }
+
+  public static int dfs(String str, int beg, int end, int k) {
+    char[] count = new char[26];
+    for (int i = beg; i <= end; i++) {
+      count[str.charAt(i) - 'a'] += 1;
+    }
+
+    char split = 0;
+    for (int i = 0; i < 26; i++) {
+      if (count[i] > 0 && count[i] < k) {
+        split = (char) (i + 'a');
+      }
+    }
+    if (split == 0) return end - beg + 1;
+
+    int ans = 0, j = beg;
+    while (j <= end) {
+      while (j <= end && str.charAt(j) == split) j++;
+      if (j > end) break;
+      int start = j;
+      while (j <= end && str.charAt(j) != split) j++;
+      int len = dfs(str, start, j - 1, k);
+      ans = Math.max(ans, len);
+    }
+
+    return ans;
   }
 
   public static void main(String[] args) {
