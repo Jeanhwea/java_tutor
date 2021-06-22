febfd1b0031da61a423c52080b8be94cc7417687
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-22 17:02:25 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/concurrent/Runner02.java
@@ -0,0 +1,96 @@
+package io.github.jeanhwea.concurrent;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * AQS 实现互斥锁 Mutex
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-22, JDK1.8
+ */
+public class Runner02 {
+
+  static class Mutex implements Lock, java.io.Serializable {
+
+    // 实现内部的帮助类
+    private static class Sync extends AbstractQueuedSynchronizer {
+
+      // 当前是否锁定
+      protected boolean isHeldExclusively() {
+        return getState() == 1;
+      }
+
+      // 获取互斥锁
+      public boolean tryAcquire(int acquires) {
+        assert acquires == 1; // Otherwise unused
+        if (compareAndSetState(0, 1)) {
+          setExclusiveOwnerThread(Thread.currentThread());
+          return true;
+        }
+        return false;
+      }
+
+      // 释放互斥锁
+      protected boolean tryRelease(int releases) {
+        assert releases == 1; // Otherwise unused
+        if (getState() == 0) throw new IllegalMonitorStateException();
+        setExclusiveOwnerThread(null);
+        setState(0);
+        return true;
+      }
+
+      // 返回条件
+      Condition newCondition() {
+        return new ConditionObject();
+      }
+
+      // Deserializes properly
+      private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+        s.defaultReadObject();
+        setState(0); // reset to unlocked state
+      }
+    }
+
+    private final Sync sync = new Sync();
+
+    public void lock() {
+      sync.acquire(1);
+    }
+
+    public boolean tryLock() {
+      return sync.tryAcquire(1);
+    }
+
+    public void unlock() {
+      sync.release(1);
+    }
+
+    public Condition newCondition() {
+      return sync.newCondition();
+    }
+
+    public boolean isLocked() {
+      return sync.isHeldExclusively();
+    }
+
+    public boolean hasQueuedThreads() {
+      return sync.hasQueuedThreads();
+    }
+
+    public void lockInterruptibly() throws InterruptedException {
+      sync.acquireInterruptibly(1);
+    }
+
+    public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
+      return sync.tryAcquireNanos(1, unit.toNanos(timeout));
+    }
+  }
+
+  public static void main(String[] args) {}
+}
---
+++ src/main/java/io/github/jeanhwea/concurrent/Runner03.java
@@ -0,0 +1,68 @@
+package io.github.jeanhwea.concurrent;
+
+import java.util.*;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @author Jinghui Hu
+ * @since 2021-06-22, JDK1.8
+ */
+public class Runner03 {
+
+  public static class BooleanLatch {
+
+    private static class Sync extends AbstractQueuedSynchronizer {
+      boolean isSignalled() {
+        return getState() != 0;
+      }
+
+      protected int tryAcquireShared(int ignore) {
+        return isSignalled() ? 1 : -1;
+      }
+
+      protected boolean tryReleaseShared(int ignore) {
+        setState(1);
+        return true;
+      }
+    }
+
+    private final Sync sync = new Sync();
+
+    public boolean isSignalled() {
+      return sync.isSignalled();
+    }
+
+    public void signal() {
+      sync.releaseShared(1);
+    }
+
+    public void await() throws InterruptedException {
+      sync.acquireSharedInterruptibly(1);
+    }
+  }
+
+  private static BooleanLatch latch = new BooleanLatch();
+
+  public static void main(String[] args) throws InterruptedException {
+    System.out.println(new Date());
+    for (int i = 0; i < 9; i++) {
+      Thread t =
+          new Thread(
+              () -> {
+                try {
+                  latch.await();
+                  Thread.sleep(1000);
+                  String name = Thread.currentThread().getName();
+                  System.out.println(name + ": " + new Date());
+                  latch.signal();
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+              });
+      t.start();
+    }
+    Thread.sleep(3000);
+    latch.signal();
+    System.out.println(new Date());
+  }
+}
