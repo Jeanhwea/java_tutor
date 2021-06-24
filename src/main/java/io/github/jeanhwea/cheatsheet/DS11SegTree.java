372026d3763a4cd937327dc869519c7a31f1df58
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-24 12:24:52 +0800

    add

--- src/main/java/io/github/jeanhwea/cheatsheet/DS11SegTree.java
+++ src/main/java/io/github/jeanhwea/cheatsheet/DS11SegTree.java
@@ -8,10 +8,18 @@ import java.util.*;
  * @author Jinghui Hu
  * @since 2021-06-24, JDK1.8
  */
-public class Ds11SegTree {
+public class DS11SegTree {
+
+  static class StNode {
+    StNode left, right, parent;
+    int val;
+
+    public StNode(int val) {
+      this.val = val;
+    }
+  }
 
   public static void main(String[] args) {
     // pass
   }
-
 }
