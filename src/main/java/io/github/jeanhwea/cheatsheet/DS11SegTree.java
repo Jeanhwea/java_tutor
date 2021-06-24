becb6f38f540db2a03d583280cb722681cec6656
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-24 11:47:38 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/cheatsheet/DS11SegTree.java
@@ -0,0 +1,17 @@
+package io.github.jeanhwea.cheatsheet;
+
+import java.util.*;
+
+/**
+ * 线段树
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-24, JDK1.8
+ */
+public class Ds11SegTree {
+
+  public static void main(String[] args) {
+    // pass
+  }
+
+}
