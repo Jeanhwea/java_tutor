4c145e4586b9295ac223789353dc6a10e2657d7e
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-25 13:47:06 +0800

    add

--- src/main/java/io/github/jeanhwea/concurrent/Runner01.java
+++ src/main/java/io/github/jeanhwea/concurrent/Runner01.java
@@ -4,19 +4,24 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Runner01 {
 
+  private static int numThread = 5;
+  private static int threshold = 99;
+
+  private static void printNum(int num) {
+    String threadName = Thread.currentThread().getName();
+    System.out.printf("%s: %2d\n", threadName, num);
+  }
+
   ////////////////////////////////////////////////////////////////////////////////
   // 不加锁（不安全操作）
   public static Integer num1 = 0;
 
   public static void test01() {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < numThread; i++) {
       Thread t =
           new Thread(
               () -> {
-                while (num1 < 999) {
-                  String threadName = Thread.currentThread().getName();
-                  System.out.printf("%03d: %s\n", num1++, threadName);
-                }
+                while (num1 < threshold) printNum(num1++);
               });
       t.start();
     }
@@ -27,15 +32,12 @@ public class Runner01 {
   public static Integer num2 = 0;
 
   public static void test02() {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < numThread; i++) {
       Thread t =
           new Thread(
               () -> {
                 synchronized (Runner01.class) {
-                  while (num1 < 999) {
-                    String threadName = Thread.currentThread().getName();
-                    System.out.printf("%03d: %s\n", num1++, threadName);
-                  }
+                  while (num2 < threshold) printNum(num2++);
                 }
               });
       t.start();
@@ -47,14 +49,11 @@ public class Runner01 {
   public static AtomicInteger num3 = new AtomicInteger(0);
 
   public static void test03() {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < numThread; i++) {
       Thread t =
           new Thread(
               () -> {
-                while (num3.get() < 999) {
-                  String threadName = Thread.currentThread().getName();
-                  System.out.printf("%03d: %s\n", num3.getAndIncrement(), threadName);
-                }
+                while (num3.get() < threshold) printNum(num3.getAndIncrement());
               });
       t.start();
     }
