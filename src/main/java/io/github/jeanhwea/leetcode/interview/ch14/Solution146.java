e31bba65e68bfa5aff4c5c7a9c9c4ac8640427db
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-28 00:13:05 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/interview/ch14/Solution146.java
+++ src/main/java/io/github/jeanhwea/leetcode/interview/ch14/Solution146.java
@@ -87,10 +87,10 @@ public class Solution146 {
 
   public static void main(String[] args) {
     LRUCache lRUCache = new LRUCache(2);
-    lRUCache.put(1, 0); // 缓存是 {1=1}
-    lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
-    System.out.println(lRUCache.get(1)); // 返回 1
-    lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+    lRUCache.put(1, 0); // 缓存是 {1=0}
+    lRUCache.put(2, 2); // 缓存是 {1=0, 2=2}
+    System.out.println(lRUCache.get(1)); // 返回 0
+    lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=0, 3=3}
     System.out.println(lRUCache.get(2)); // 返回 -1 (未找到)
     lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
     System.out.println(lRUCache.get(1)); // 返回 -1 (未找到)
