5110bd6609830cf35952b0fabe931b18368bda17
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-27 14:05:35 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/interview/ch04/Solution212.java
+++ src/main/java/io/github/jeanhwea/leetcode/interview/ch04/Solution212.java
@@ -10,7 +10,8 @@ import java.util.*;
  */
 public class Solution212 {
 
-  private static Set<String> dict;
+  private static Trie trie;
+  // private static Set<String> dict;
   private static List<String> ans;
   private static int[][] seen;
   private static List<Character> choose;
@@ -18,8 +19,10 @@ public class Solution212 {
   private static int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
 
   public static List<String> findWords(char[][] board, String[] words) {
-    dict = new HashSet<>();
-    for (int i = 0; i < words.length; i++) dict.add(words[i]);
+    trie = new Trie();
+    // dict = new HashSet<>();
+    // for (int i = 0; i < words.length; i++) dict.add(words[i]);
+    for (int i = 0; i < words.length; i++) trie.insert(words[i]);
     ans = new LinkedList<>();
     n = board.length;
     m = board[0].length;
@@ -40,11 +43,10 @@ public class Solution212 {
 
     char ch = board[x][y];
     String w = "";
-    for (int i = 0; i < choose.size(); i++) {
-      w += choose.get(i);
-    }
+    for (int i = 0; i < choose.size(); i++) w += choose.get(i);
     w += ch;
-    if (dict.contains(w) && !ans.contains(w)) ans.add(w);
+    if (trie.search(w) && !ans.contains(w)) ans.add(w);
+    if (!trie.startsWith(w)) return;
 
     for (int i = 0; i < 4; i++) {
       int x1 = x + dx[i], y1 = y + dy[i];
@@ -56,17 +58,62 @@ public class Solution212 {
     }
   }
 
+  public static class Trie {
+
+    private boolean isLeaf;
+    private Trie[] children;
+
+    public Trie() {
+      this.isLeaf = false;
+      this.children = new Trie[26];
+    }
+
+    public void insert(String word) {
+      Trie p = this;
+      int n = word.length();
+      for (int i = 0; i < n; i++) {
+        int k = word.charAt(i) - 'a';
+        if (p.children[k] == null) {
+          p.children[k] = new Trie();
+        }
+        p = p.children[k];
+      }
+      p.isLeaf = true;
+    }
+
+    public boolean search(String word) {
+      Trie p = searchPrefix(word);
+      return p != null && p.isLeaf;
+    }
+
+    public boolean startsWith(String prefix) {
+      Trie p = searchPrefix(prefix);
+      return p != null;
+    }
+
+    private Trie searchPrefix(String prefix) {
+      Trie p = this;
+      int n = prefix.length();
+      for (int i = 0; i < n; i++) {
+        int k = prefix.charAt(i) - 'a';
+        if (p.children[k] == null) return null;
+        p = p.children[k];
+      }
+      return p;
+    }
+  }
+
   public static void main(String[] args) {
-    // char[][] board = {
-    //   {'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}
-    // };
-    // String[] words = {"oath", "pea", "eat", "rain"};
+    char[][] board = {
+      {'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}
+    };
+    String[] words = {"oath", "pea", "eat", "rain"};
 
     // char[][] board = {{'a'}};
     // String[] words = {"a"};
 
-    char[][] board = {{'a', 'b'}, {'c', 'd'}};
-    String[] words = {"abcd"};
+    // char[][] board = {{'a', 'b'}, {'c', 'd'}};
+    // String[] words = {"abcd"};
 
     List<String> res = findWords(board, words);
     System.out.println(res);
