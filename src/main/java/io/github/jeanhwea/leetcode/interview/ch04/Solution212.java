a6b0ae31eb74413d70a8a59708a4414505167db7
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-27 16:37:14 +0800

    ac: 单词搜索 II

--- src/main/java/io/github/jeanhwea/leetcode/interview/ch04/Solution212.java
+++ src/main/java/io/github/jeanhwea/leetcode/interview/ch04/Solution212.java
@@ -14,46 +14,45 @@ public class Solution212 {
   // private static Set<String> dict;
   private static List<String> ans;
   private static int[][] seen;
-  private static List<Character> choose;
+  private static StringBuffer c;
+  private static char[][] a;
   private static int n, m;
   private static int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
 
   public static List<String> findWords(char[][] board, String[] words) {
     trie = new Trie();
-    // dict = new HashSet<>();
-    // for (int i = 0; i < words.length; i++) dict.add(words[i]);
     for (int i = 0; i < words.length; i++) trie.insert(words[i]);
     ans = new LinkedList<>();
+    a = board;
     n = board.length;
     m = board[0].length;
+
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         seen = new int[n][m];
-        choose = new LinkedList<>();
-        dfs(board, i, j);
+        c = new StringBuffer();
+        dfs(i, j);
       }
     }
 
     return ans;
   }
 
-  private static void dfs(char[][] board, int x, int y) {
+  private static void dfs(int x, int y) {
     if (x < 0 || x >= n || y < 0 || y >= m) return;
     if (seen[x][y] == 1) return;
 
-    char ch = board[x][y];
-    String w = "";
-    for (int i = 0; i < choose.size(); i++) w += choose.get(i);
-    w += ch;
+    char ch = a[x][y];
+    String w = c.toString() + ch;
     if (trie.search(w) && !ans.contains(w)) ans.add(w);
     if (!trie.startsWith(w)) return;
 
     for (int i = 0; i < 4; i++) {
       int x1 = x + dx[i], y1 = y + dy[i];
-      choose.add(ch);
+      c.append(ch);
       seen[x][y] = 1;
-      dfs(board, x1, y1);
-      choose.remove(choose.size() - 1);
+      dfs(x1, y1);
+      c.deleteCharAt(c.length() - 1);
       seen[x][y] = 0;
     }
   }
