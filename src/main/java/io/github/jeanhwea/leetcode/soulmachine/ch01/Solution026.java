e44df203f4d364c90583ec496ca1511ff15a34d3
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-28 16:30:37 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/soulmachine/ch01/Solution026.java
@@ -0,0 +1,30 @@
+package io.github.jeanhwea.leetcode.soulmachine.ch01;
+
+import java.util.*;
+
+/**
+ * 删除有序数组中的重复项
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-28, JDK1.8
+ */
+public class Solution026 {
+
+  public static int removeDuplicates(int[] a) {
+    int n = a.length, k = 0;
+    for (int i = 1; i < n; i++) {
+      if (a[i] != a[k]) {
+        a[++k] = a[i];
+      }
+    }
+    return k + 1;
+  }
+
+  public static void main(String[] args) {
+    int[] a = {1, 2, 2, 3};
+    // int[] a = {1, 1, 1, 2};
+    int k = removeDuplicates(a);
+    int[] b = Arrays.copyOfRange(a, 0, k);
+    System.out.println(Arrays.toString(b));
+  }
+}
