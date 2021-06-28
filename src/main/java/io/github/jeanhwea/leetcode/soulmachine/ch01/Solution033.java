0eda51e98d20065dee0dd80a485c618396190223
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-28 21:17:34 +0800

    add: 旋转数组二分查找

--- src/main/java/io/github/jeanhwea/leetcode/soulmachine/ch01/Solution033.java
+++ src/main/java/io/github/jeanhwea/leetcode/soulmachine/ch01/Solution033.java
@@ -9,29 +9,28 @@ package io.github.jeanhwea.leetcode.soulmachine.ch01;
 public class Solution033 {
 
   /**
-   * 将数组一分为二，必定一个子数组有序，判断 k 是否在有序组中来进行二分查找
+   * 将数组一分为二，必定一个子数组有序，判断 target 是否在有序组中来进行二分查找
    *
    * @author Jinghui Hu
    * @since 2021-06-20, JDK1.8
    */
-  public static int search(int[] a, int k) {
-    int n = a.length, lo = 0, hi = n - 1, mid;
-    while (lo <= hi) {
-      mid = lo + (hi - lo) / 2;
-      // System.out.printf("lo=%d,hi=%d\n", lo, hi);
-      if (a[mid] == k) return mid;
+  public static int search(int[] a, int target) {
+    int n = a.length, beg = 0, end = n - 1;
+    while (beg <= end) {
+      int mid = beg + (end - beg) / 2;
+      if (a[mid] == target) return mid;
 
-      if (a[lo] <= a[mid]) {
-        if (a[lo] <= k && k < a[mid]) {
-          hi = mid;
+      if (a[0] <= a[mid]) {
+        if (a[0] <= target && target < a[mid]) {
+          end = mid - 1;
         } else {
-          lo = mid + 1;
+          beg = mid + 1;
         }
       } else {
-        if (a[mid] < k && k <= a[hi]) {
-          lo = mid + 1;
+        if (a[mid] < target && target <= a[n - 1]) {
+          beg = mid + 1;
         } else {
-          hi = mid;
+          end = mid - 1;
         }
       }
     }
@@ -39,7 +38,7 @@ public class Solution033 {
   }
 
   public static void main(String[] args) {
-    int[] a = {5, 1, 3};
-    System.out.println(search(a, 3));
+    int[] a = {3, 1};
+    System.out.println(search(a, 1));
   }
 }
