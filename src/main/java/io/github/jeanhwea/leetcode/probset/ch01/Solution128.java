5e87f7b70e8811ab55fc3d7a006bd2588760967b
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-29 22:27:34 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch01/Solution128.java
@@ -0,0 +1,22 @@
+package io.github.jeanhwea.leetcode.probset.ch01;
+
+import java.util.*;
+
+/**
+ * 最长连续序列
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-29, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution128 {
+
+  public static int longestConsecutive(int[] a) {
+    //
+  }
+
+  public static void main(String[] args) {
+    int[] a = {100, 4, 200, 1, 3, 2};
+    System.out.println(longestConsecutive(a));
+  }
+}
