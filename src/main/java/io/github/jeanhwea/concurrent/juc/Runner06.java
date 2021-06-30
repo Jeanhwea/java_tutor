5f6a51493b8120320e20b05fa0218a9f289f0f22
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-30 21:09:01 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/concurrent/juc/Runner06.java
@@ -0,0 +1,23 @@
+package io.github.jeanhwea.concurrent.juc;
+
+public class Runner06 {
+
+  // 这里如果不添加 volatile 关键字子线程只能读到 flag 为 true
+  private static /* volatile */ boolean flag = true;
+
+  public static void main(String[] args) throws InterruptedException {
+    Thread t =
+        new Thread(
+            () -> {
+              while (flag) {
+                // 死循环，线程不能终止
+              }
+              System.out.println("done");
+            });
+    t.start();
+    Thread.sleep(500L);
+
+    System.out.println("main");
+    flag = false;
+  }
+}
