4604a60a681005a5b7b61b31332fba1d5066ddb7
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-06-30 09:07:04 +0800

    add: 添加 ThreadLocal 问题

--- src/main/java/io/github/jeanhwea/concurrent/thd/Runner01.java
+++ src/main/java/io/github/jeanhwea/concurrent/thd/Runner01.java
@@ -1,5 +1,79 @@
 package io.github.jeanhwea.concurrent.thd;
 
+/**
+ * ThreadLocal 类的方法
+ *
+ * @author Jinghui Hu
+ * @since 2021-06-30, JDK1.8
+ */
 public class Runner01 {
 
+  public static class Person01 {
+    private static String name;
+
+    public String getName() {
+      return this.name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+  }
+
+  public static class Person02 {
+    private static ThreadLocal<String> name = new ThreadLocal<>();
+
+    public String getName() {
+      return this.name.get();
+    }
+
+    public void setName(String name) {
+      this.name.set(name);
+    }
+  }
+
+  public static void test01() {
+    for (int i = 0; i < 5; i++) {
+      Thread t =
+          new Thread(
+              () -> {
+                try {
+                  String threadName = Thread.currentThread().getName();
+                  Person01 person01 = new Person01();
+                  person01.setName(threadName);
+                  Thread.sleep(500L);
+                  System.out.printf(
+                      "hash=%s,name=%s\n", person01.getName().hashCode(), person01.getName());
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+              });
+      t.start();
+    }
+  }
+
+  public static void test02() {
+    for (int i = 0; i < 5; i++) {
+      Thread t =
+          new Thread(
+              () -> {
+                try {
+                  String threadName = Thread.currentThread().getName();
+                  Person02 person02 = new Person02();
+                  person02.setName(threadName);
+                  Thread.sleep(500L);
+                  System.out.printf(
+                      "hash=%s,name=%s\n", person02.getName().hashCode(), person02.getName());
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+              });
+      t.start();
+    }
+  }
+
+  public static void main(String[] args) {
+    test01();
+    test02();
+  }
 }
