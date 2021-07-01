3d5fc331744133596ab0bc4ff7287357d48768e8
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-01 11:32:23 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch01/Solution015.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch01/Solution015.java
@@ -12,27 +12,26 @@ import java.util.*;
 public class Solution015 {
 
   public static List<List<Integer>> threeSum(int[] a) {
-    int n = a.length;
-    if (n < 3) return null;
-
-    Arrays.sort(a);
     List<List<Integer>> ans = new LinkedList<>();
-
-    for (int i = 0; i < n; i++) {
-      while (i > 0 && a[i] == a[i - 1]) i++;
-
-      int target = -a[i];
-      for (int j = n - 1; j >= 0; j--) {
-        while (j < n - 1 && a[j] == a[j + 1]) j--;
-
-        for (int k = i + 1; k < j; k++) {
-          if (a[j] + a[k] == target) {
+    int n = a.length;
+    if (n < 3) return ans;
+    Arrays.sort(a);
+    // System.out.println(Arrays.toString(a));
+    int i = 0, j = n - 1, k = 0;
+    for (i = 0; i < n - 1; i++) {
+      while (0 < i && i < n - 1 && a[i] == a[i - 1]) i++;
+      for (j = n - 1; j > i; j--) {
+        while (i < j && j < n - 1 && a[j] == a[j + 1]) j--;
+        for (k = i + 1; k < j; k++) {
+          while (k < j - 1 && a[k] == a[k + 1]) k++;
+          // System.out.printf("i,k,j=%d,%d,%d\n", i, k, j);
+          if (k != j && a[i] + a[k] + a[j] == 0) {
             ans.add(Arrays.asList(a[i], a[k], a[j]));
+            break;
           }
         }
       }
     }
-
     return ans;
   }
 
@@ -41,7 +40,7 @@ public class Solution015 {
     Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < n; i++) map.put(a[i], i);
 
-    // List<List<Integer>> ans = new LinkedList<>();
+    List<List<Integer>> ans = new LinkedList<>();
     Set<List<Integer>> set = new HashSet<>();
     for (int i = 0; i < n; i++) {
       for (int j = i + 1; j < n; j++) {
@@ -58,7 +57,10 @@ public class Solution015 {
   }
 
   public static void main(String[] args) {
-    int[] a = {-1, 0, 1, 2, -1, -4};
+    // int[] a = {-1, 0, 1, 2, -1, -4};
+    // int[] a = {0, 0, 0, 0};
+    int[] a = {0, 0, 0};
+    // int[] a = {-1, 0, 1, 0};
     System.out.println(threeSum(a));
   }
 }
