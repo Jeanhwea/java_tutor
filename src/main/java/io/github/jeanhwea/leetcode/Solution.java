4e37ff5af683dfe2d5fe2ed3bbaa480a643cafbe
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-01 20:18:40 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/Solution.java
+++ src/main/java/io/github/jeanhwea/leetcode/Solution.java
@@ -5,38 +5,30 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Solution {
 
-  public static void threeSum(int[] a, int target) {
-    int n = a.length;
-    List<Integer> choose = new LinkedList<>();
-    Set<List<Integer>> ans = new HashSet<>();
-    dfs(a, 0, target, choose, 0, ans);
-    System.out.println(ans);
+  public static void main(String[] args) {
+    int[][] a = {
+      {1, 2, 3, 4},
+      {5, 6, 7, 8},
+      {9, 10, 11, 12}
+    };
+    printMatrix(a);
   }
 
-  public static void dfs(
-      int[] a, int k, int target, List<Integer> choose, int currSum, Set<List<Integer>> ans) {
-    int n = a.length;
-    if (choose.size() == 3 && currSum == target) {
-      ans.add(new ArrayList<>(choose));
-      return;
+  public static void printMatrix(int[][] a) {
+    int n = a.length, m = a[0].length;
+
+    for (int i = 0; i < n; i++) {
+      int x = n, y = i;
+      while (y >= 0 && x >= 0) {
+        System.out.println(a[y--][x--]);
+      }
     }
 
-    // purge
-    if (currSum > 10 || k >= n) return;
-
-    // if not choose a[k]
-    dfs(a, k + 1, target, choose, currSum, ans);
-
-    // if choose a[k]
-    choose.add(a[k]);
-    currSum += a[k];
-    dfs(a, k + 1, target, choose, currSum, ans);
-    choose.remove(choose.size() - 1);
-    currSum -= a[k];
-  }
-
-  public static void main(String[] args) {
-    int[] a = {1, 3, 0, 4, 2, 3, 9, 3, 0, 7, 6};
-    threeSum(a, 10);
+    for (int i = m - 1; i >= 0; i--) {
+      int x = i - 1, y = n - 1;
+      while (y >= 0 && x >= 0) {
+        System.out.println(a[y--][x--]);
+      }
+    }
   }
 }
