3b744f3900bf4acb70672934f03e034cc743287a
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-02 23:40:24 +0800

    add

--- query/test01.sql
+++ query/test01.sql
@@ -1,1 +1,7 @@
 show databases;
+
+create database test01 default character set utf8mb4 collate utf8mb4_general_ci;
+
+create user 'user01'@'%' identified by 'user01';
+grant all privileges on *.* to 'user01'@'%';
+flush privileges;
