348f4491a4e03529fec2aac1663619172372e377
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-02 23:48:54 +0800

    add

--- query/test01.sql
+++ query/test01.sql
@@ -5,3 +5,23 @@ create database test01 default character set utf8mb4 collate utf8mb4_general_ci;
 create user 'user01'@'%' identified by 'user01';
 grant all privileges on *.* to 'user01'@'%';
 flush privileges;
+
+
+drop table author;
+
+create table author (
+  id int auto_increment primary key,
+  name varchar(128) not null,
+  age int,
+  gender varchar(32)
+);
+
+desc author;
+
+show create table author\G
+
+select * from author;
+
+insert into author('aaa', 10, 'M');
+
+commit;
