1cf49e745bb0c0e06b63c716922fe24d5564e2ca
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-03 17:24:49 +0800

    add

--- src/main/java/io/github/jeanhwea/lang/jvm/Example01.java
+++ src/main/java/io/github/jeanhwea/lang/jvm/Example01.java
@@ -11,7 +11,18 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Example01 {
 
-  public static void main(String[] args) {
+  private static final int FINAL_VAR01 = 2;
+  private String name = "hello";
+
+  public int method01() {
+    int a = 1;
+    int b = 2;
+    int c = (a + b) / 5;
+    return c;
   }
 
+  public static void main(String[] args) {
+    Example01 example = new Example01();
+    example.method01();
+  }
 }
