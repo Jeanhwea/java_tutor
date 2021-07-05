058c6954b2962d742db30ac443204b3ecdcc9a46
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-06 00:08:24 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch01/Solution060.java
@@ -0,0 +1,23 @@
+package io.github.jeanhwea.leetcode.probset.ch01;
+
+import java.util.*;
+
+/**
+ * 排列序列
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-06, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution060 {
+
+  public static String getPermutation(int n, int k) {
+    //
+    return null;
+  }
+
+  public static void main(String[] args) {
+    int n = 3, k = 3;
+    System.out.println(getPermutation(n, k));
+  }
+}
