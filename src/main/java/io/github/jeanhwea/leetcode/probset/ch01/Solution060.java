74a0b5ab301bd033494937606935aff6b94d0305
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-06 22:21:34 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch01/Solution060.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch01/Solution060.java
@@ -11,9 +11,40 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Solution060 {
 
+  /**
+   * 康托编码
+   *
+   * @author Jinghui Hu
+   * @since 2021-07-06, JDK1.8
+   */
   public static String getPermutation(int n, int k) {
-    //
-    return null;
+    int[] fact = new int[n];
+    fact[0] = 1;
+    for (int i = 1; i < n; i++) {
+      fact[i] = i * fact[i - 1];
+    }
+
+    int[] seen = new int[n + 1];
+    Arrays.fill(seen, 0);
+    --k;
+    StringBuilder sb = new StringBuilder();
+    while (true) {
+      int groupLength = fact[n - sb.length() - 1];
+      int count = k / groupLength;
+      for (int i = 1; i <= n; i++) {
+        if (seen[i] == 0) count--;
+        if (count < 0) {
+          seen[i] = 1;
+          sb.append(i);
+          break;
+        }
+      }
+      k -= groupLength * (k / groupLength);
+
+      if (sb.length() == n) break;
+    }
+
+    return sb.toString();
   }
 
   public static void main(String[] args) {
