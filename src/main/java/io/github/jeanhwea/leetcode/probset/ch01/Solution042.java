96ca063e771b0eade3d172198a921a01195e5e35
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-07 19:20:22 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch01/Solution042.java
@@ -0,0 +1,54 @@
+package io.github.jeanhwea.leetcode.probset.ch01;
+
+import java.util.*;
+
+/**
+ * 接雨水
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-07, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution042 {
+
+  public static int trap(int[] height) {
+    int n = height.length;
+    PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> y[1] - x[1]);
+    for (int i = 0; i < n; i++) {
+      pq.offer(new int[] {i, height[i]});
+    }
+
+    int ans = 0, prev = -1, curr = -1;
+    List<Integer> list = new ArrayList<>();
+    while (!pq.isEmpty()) {
+      int[] e = pq.poll();
+
+      curr = e[1];
+      if (prev < 0) prev = curr;
+      list.add(e[0]);
+      while (!pq.isEmpty() && pq.peek()[1] == curr) {
+        list.add(pq.poll()[0]);
+      }
+      Collections.sort(list);
+      System.out.println(list);
+
+      int left = list.get(0);
+      int count = 0;
+      for (int i = 1; i < list.size(); i++) {
+        count += list.get(i) - left - 1;
+        left = list.get(i);
+      }
+      // System.out.println(count);
+      System.out.printf("count,c,p=%d,%d,%d\n", count, prev, curr);
+      ans += count * (prev - curr);
+      prev = curr;
+    }
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    // int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    int[] height = {4, 2, 0, 3, 2, 5};
+    System.out.println(trap(height));
+  }
+}
