4aa18736469ff98adc09d6542928a0ad39169aaf
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-07 11:46:50 +0800

    add

--- src/main/java/io/github/jeanhwea/lang/juc/Runner06.java
+++ src/main/java/io/github/jeanhwea/lang/juc/Runner06.java
@@ -1,11 +1,13 @@
 package io.github.jeanhwea.lang.juc;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Runner06 {
 
   // 这里如果不添加 volatile 关键字子线程只能读到 flag 为 true
   private static /* volatile */ boolean flag = true;
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void test01() throws InterruptedException {
     Thread t =
         new Thread(
             () -> {
@@ -19,5 +21,36 @@ public class Runner06 {
 
     System.out.println("main");
     flag = false;
+  }
+
+  private static int count = 0;
+
+  public static void test02() throws InterruptedException {
+    int nThread = 9;
+    CountDownLatch countDownLatch = new CountDownLatch(nThread);
+    for (int i = 0; i < nThread; i++) {
+      Thread t =
+          new Thread(
+              () -> {
+                try {
+                  count += 1;
+                  System.out.println("count=" + count);
+                  Thread.sleep(3000);
+                  countDownLatch.countDown();
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+              });
+      t.start();
+    }
+
+    // Thread.sleep(1000);
+    countDownLatch.await();
+    System.out.println(count);
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    // test01();
+    test02();
   }
 }
