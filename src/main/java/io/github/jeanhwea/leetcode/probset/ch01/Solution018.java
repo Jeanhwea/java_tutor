a39e569fd4d6e460e6cd368dfbcdf7c885f91daa
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-07 10:57:58 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch01/Solution018.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch01/Solution018.java
@@ -14,7 +14,8 @@ public class Solution018 {
   public static List<List<Integer>> fourSum(int[] a, int target) {
     Arrays.sort(a);
     int n = a.length;
-    List<List<Integer>> ans = new LinkedList<>();
+
+    Set<List<Integer>> ans = new HashSet<>();
     for (int i = 0; i < n; i++) {
       for (int j = i + 1; j < n; j++) {
         int sum = target - a[i] - a[j];
@@ -22,7 +23,7 @@ public class Solution018 {
         for (int x = j + 1; x < y; x++) {
           if (x != j + 1 && a[x] == a[x - 1]) continue;
 
-          while (a[y] + a[x] > sum) y--;
+          while (y > x && a[y] + a[x] > sum) y--;
 
           if (y > x && a[y] + a[x] == sum) {
             ans.add(Arrays.asList(a[i], a[j], a[x], a[y]));
@@ -31,13 +32,15 @@ public class Solution018 {
       }
     }
 
-    return ans;
+    return new LinkedList<>(ans);
   }
 
   public static void main(String[] args) {
     // int[] a = {1, 0, -1, 0, -2, 2};
     // System.out.println(fourSum(a, 0));
-    int[] a = {2, 2, 2, 2, 2};
-    System.out.println(fourSum(a, 8));
+    // int[] a = {2, 2, 2, 2, 2};
+    // int[] a = {-3, -1, 0, 2, 4, 5};
+    int[] a = {-2, -1, -1, 1, 1, 2, 2};
+    System.out.println(fourSum(a, 0));
   }
 }
