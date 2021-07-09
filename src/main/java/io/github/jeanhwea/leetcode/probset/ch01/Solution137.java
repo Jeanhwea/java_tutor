c7c729d39591202b01ba66b251c3b4abdcc196f0
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-09 08:38:26 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch01/Solution137.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch01/Solution137.java
@@ -11,7 +11,20 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Solution137 {
 
-  public static void main(String[] args) {
+  public static int singleNumber(int[] a) {
+    Map<Integer, Integer> count = new HashMap<>();
+    int n = a.length;
+    for (int i = 0; i < n; i++) {
+      count.put(a[i], count.getOrDefault(a[i], 0) + 1);
+    }
+    for (Map.Entry<Integer, Integer> e : count.entrySet()) {
+      if (e.getValue() == 1) return e.getKey();
+    }
+    return -1;
   }
 
+  public static void main(String[] args) {
+    int[] a = {2, 2, 3, 2};
+    System.out.println(singleNumber(a));
+  }
 }
