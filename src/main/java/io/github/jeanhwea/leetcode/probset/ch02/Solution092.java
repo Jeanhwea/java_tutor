3c33afcf629b2a5be1562454442d7fc262a59691
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-10 09:29:55 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch02/Solution092.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch02/Solution092.java
@@ -12,7 +12,36 @@ import java.util.*;
 public class Solution092 {
 
   public static ListNode reverseBetween(ListNode head, int left, int right) {
-    ListNode p;
+    ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+
+    ListNode p = dummy;
+    int i = 0;
+    while (i++ < left - 1) p = p.next;
+
+    ListNode t1 = p;
+    i++;
+    p = p.next;
+
+    ListNode h2 = null, t2 = null;
+    while (p != null && i <= right + 1) {
+      if (h2 == null) {
+        h2 = t2 = p;
+        p = p.next;
+        t2.next = null;
+      } else {
+        ListNode t = h2;
+        h2 = p;
+        p = p.next;
+        h2.next = t;
+      }
+      i++;
+    }
+
+    t1.next = h2;
+    t2.next = p;
+
+    return dummy.next;
   }
 
   public static void main(String[] args) {
