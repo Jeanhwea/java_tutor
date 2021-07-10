cac7ec01accae59b7a2d89eb0dfd2a5bac7bc172
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-10 12:09:21 +0800

    add: 删除排序链表中的重复元素 II

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch02/Solution082.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch02/Solution082.java
@@ -11,7 +11,28 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Solution082 {
 
-  public static void main(String[] args) {
+  public static ListNode deleteDuplicates(ListNode head) {
+    ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+
+    ListNode p = dummy;
+    while (p != null) {
+      ListNode q = p.next;
+      while (q != null && q.next != null && q.val == q.next.val) {
+        q = q.next;
+      }
+      if (q != p.next) {
+        p.next = q.next;
+      } else {
+        p = p.next;
+      }
+    }
+
+    return dummy.next;
   }
 
+  public static void main(String[] args) {
+    ListNode list01 = ListNode.makeList(new int[] {1, 2, 3, 3, 4, 4, 5, 5});
+    ListNode.dispList(deleteDuplicates(list01));
+  }
 }
