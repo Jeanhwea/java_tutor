b485f5c94bd7910e8c24a5c9ea0504f9c531b166
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-10 13:49:32 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch02/Solution138.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch02/Solution138.java
@@ -9,13 +9,49 @@ import java.util.*;
  * @since 2021-07-10, JDK1.8
  */
 @SuppressWarnings("all")
-public class Solution1
-    public static Node copyRandomList(Node head) {
-  // pass
+public class Solution138 {
+
+  private static class Node {
+    int val;
+    Node next, random;
+
+    Node() {}
+
+    Node(int val) {
+      this.val = val;
     }
 
-  public static void main(String[] args) {
-    // pass
+    Node(int val, Node next) {
+      this.val = val;
+      this.next = next;
+    }
   }
 
+  public static Node copyRandomList(Node head) {
+    Map<Node, Node> tb = new HashMap<>();
+    Node p = head, head2 = null, q = null;
+    while (p != null) {
+      if (head2 == null) {
+        head2 = q = new Node(p.val);
+      } else {
+        q = q.next = new Node(p.val);
+      }
+      tb.put(p, q);
+      p = p.next;
+    }
+
+    p = head;
+    q = head2;
+    while (p != null) {
+      q.random = tb.get(p.random);
+      p = p.next;
+      q = q.next;
+    }
+
+    return head2;
+  }
+
+  public static void main(String[] args) {
+    //
+  }
 }
