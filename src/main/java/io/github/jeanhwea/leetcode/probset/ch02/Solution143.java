44df34b4197d99714e86661241ffe8f68c01c530
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-10 19:12:40 +0800

    add: 添加链表重排

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch02/Solution143.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch02/Solution143.java
@@ -12,6 +12,36 @@ import java.util.*;
 public class Solution143 {
 
   public static void reorderList(ListNode head) {
+    // 快慢指针将链表划分
+    ListNode slow = head, fast = head;
+    while (fast != null && fast.next != null && fast.next.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    // 链表拆分，后半部分需要逆序
+    ListNode dummy = new ListNode(-1);
+    ListNode p = slow, q = null;
+    while (p != null) {
+      q = dummy.next;
+      dummy.next = p;
+      p = p.next;
+      dummy.next.next = q;
+    }
+
+    // 合并两个链表
+    p = head;
+    q = dummy.next;
+    while (p != null && q != null) {
+      ListNode t = p.next;
+      p.next = q;
+      q = q.next;
+      p.next.next = t;
+      p = t;
+    }
+  }
+
+  public static void reorderList0(ListNode head) {
     // 计算链表大小
     int n = 0;
     ListNode p = head, q = null;
