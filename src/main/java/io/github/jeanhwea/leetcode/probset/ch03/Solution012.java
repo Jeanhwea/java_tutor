edf88c8ad760a51da5b038a00e51d632b19d10f6
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-11 18:43:21 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch03/Solution012.java
@@ -0,0 +1,54 @@
+package io.github.jeanhwea.leetcode.probset.ch03;
+
+import java.util.*;
+
+/**
+ * 整数转罗马数字
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-11, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution012 {
+
+  private static Map<Integer, String> map =
+      new HashMap<Integer, String>() {
+        {
+          put(1, "I");
+          put(5, "V");
+          put(10, "X");
+          put(50, "L");
+          put(100, "C");
+          put(500, "D");
+          put(1000, "M");
+          put(4, "IV");
+          put(9, "IX");
+          put(40, "XL");
+          put(90, "XC");
+          put(400, "CD");
+          put(900, "CM");
+        }
+      };
+
+  public static String intToRoman(int num) {
+    List<Integer> bases = new ArrayList<>(map.keySet());
+    Collections.sort(bases);
+    System.out.println(bases);
+
+    StringBuilder sb = new StringBuilder();
+    int i = bases.size() - 1;
+    while (num > 0) {
+      int base = bases.get(i--);
+      int count = num / base;
+      for (int j = 0; j < count; j++) {
+        sb.append(map.get(base));
+      }
+      num -= base * count;
+    }
+    return sb.toString();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(intToRoman(58));
+  }
+}
---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch03/Solution014.java
@@ -0,0 +1,36 @@
+package io.github.jeanhwea.leetcode.probset.ch03;
+
+import java.util.*;
+
+/**
+ * 最长公共前缀
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-11, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution014 {
+
+  public static String longestCommonPrefix(String[] strs) {
+    int n = strs.length, m = strs[0].length(), k = 0;
+
+    done:
+    for (int i = 0; i < m; i++) {
+      char ch = strs[0].charAt(i);
+      boolean same = true;
+      for (int j = 1; j < n; j++) {
+        if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
+          break done;
+        }
+      }
+      k++;
+    }
+
+    return strs[0].substring(0, k);
+  }
+
+  public static void main(String[] args) {
+    String[] strs = new String[] {"flower", "flower", "flower", "flower"};
+    System.out.println(longestCommonPrefix(strs));
+  }
+}
