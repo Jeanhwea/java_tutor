2ed716e8c500500cf38912e2c2ada0bc58e50c91
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-11 12:36:12 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch03/Solution005.java
@@ -0,0 +1,21 @@
+package io.github.jeanhwea.leetcode.probset.ch03;
+
+import java.util.*;
+
+/**
+ * 最长回文子串
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-11, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution005 {
+
+  public static String longestPalindrome(String s) {
+    //
+  }
+
+  public static void main(String[] args) {
+    System.out.println(longestPalindrome("babad"));
+  }
+}
