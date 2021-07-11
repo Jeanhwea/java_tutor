515f40e66ae80c389be82294abfda824512644dc
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-11 18:49:11 +0800

    add: 添加最后一个单词长度

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch03/Solution058.java
@@ -0,0 +1,27 @@
+package io.github.jeanhwea.leetcode.probset.ch03;
+
+import java.util.*;
+
+/**
+ * 最后一个单词的长度
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-11, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution058 {
+
+  public static int lengthOfLastWord(String s) {
+    int i = s.length() - 1;
+    while (i >= 0 && !Character.isLetter(s.charAt(i))) i--;
+
+    int ans = 0;
+    while (i >= 0 && Character.isLetter(s.charAt(i--))) ans++;
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    String s = "Hello world+";
+    System.out.println(lengthOfLastWord(s));
+  }
+}
