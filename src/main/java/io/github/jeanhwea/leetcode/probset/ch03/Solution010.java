381bd17ee41cbc9ab17d7883fa70c84e8bef1134
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-11 22:21:24 +0800

    add: 正则表达式匹配

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch03/Solution010.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch03/Solution010.java
@@ -11,9 +11,39 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Solution010 {
 
-  public boolean isMatch(String s, String p) {
-    //
-    return true;
+  public static boolean isMatch(String s, String p) {
+    int n = s.length(), m = p.length();
+    // dp[i][j] 表示字串 s[0..i-1] 和字串 p[0..j-1] 是否匹配
+    boolean[][] dp = new boolean[n + 1][m + 1];
+
+    // 准备初值
+    dp[0][0] = true;
+    for (int j = 1; j <= m; j++) {
+      if (p.charAt(j - 1) == '*') {
+        dp[0][j] = dp[0][j - 2];
+      }
+    }
+
+    // 状态转换
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+        if (p.charAt(j - 1) == '*') {
+          if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+            dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+          } else {
+            dp[i][j] = dp[i][j - 2];
+          }
+        } else {
+          if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
+            dp[i][j] = dp[i - 1][j - 1];
+          } else {
+            dp[i][j] = false;
+          }
+        }
+      }
+    }
+
+    return dp[n][m];
   }
 
   public static void main(String[] args) {
