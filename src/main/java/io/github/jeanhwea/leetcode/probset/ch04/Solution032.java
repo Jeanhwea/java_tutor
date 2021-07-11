74b18b2add6c55cf16689c2aeb2e53ba6c5d4fe1
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-11 22:33:46 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch04/Solution032.java
@@ -0,0 +1,21 @@
+package io.github.jeanhwea.leetcode.probset.ch04;
+
+import java.util.*;
+
+/**
+ * 最长有效括号
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-11, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution032 {
+    public static int longestValidParentheses(String s) {
+
+    }
+
+  public static void main(String[] args) {
+    System.out.println(longestValidParentheses("(()"));
+  }
+
+}
