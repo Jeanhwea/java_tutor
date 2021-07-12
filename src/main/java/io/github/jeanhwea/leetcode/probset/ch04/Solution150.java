4d1139360fc321243a988ca5da1fbdb42dccd444
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-12 14:02:32 +0800

    add: 添加逆波兰操作数

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch04/Solution150.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch04/Solution150.java
@@ -12,8 +12,37 @@ import java.util.*;
 public class Solution150 {
 
   public static int evalRPN(String[] tokens) {
-    int ans = 0;
-    return ans;
+    int n = tokens.length, val1 = 0, val2 = 0;
+    Deque<Integer> val = new LinkedList<>();
+    for (int i = 0; i < n; i++) {
+      String token = tokens[i];
+      switch (token) {
+        case "+":
+          val2 = val.pop();
+          val1 = val.pop();
+          val.push(val1 + val2);
+          break;
+        case "-":
+          val2 = val.pop();
+          val1 = val.pop();
+          val.push(val1 - val2);
+          break;
+        case "*":
+          val2 = val.pop();
+          val1 = val.pop();
+          val.push(val1 * val2);
+          break;
+        case "/":
+          val2 = val.pop();
+          val1 = val.pop();
+          val.push(val1 / val2);
+          break;
+        default:
+          val.push(Integer.parseInt(token));
+          break;
+      }
+    }
+    return val.pop();
   }
 
   public static void main(String[] args) {
