be3a79ecf816ae43809aca3d98a8c30e8a5bc6fd
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-12 13:52:19 +0800

    add: 添加柱状图中最大的矩形

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch04/Solution084.java
@@ -0,0 +1,17 @@
+package io.github.jeanhwea.leetcode.probset.ch04;
+
+import java.util.*;
+
+/**
+ * 柱状图中最大的矩形
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-12, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution084 {
+
+  public static void main(String[] args) {
+  }
+
+}
---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch04/Solution150.java
@@ -0,0 +1,23 @@
+package io.github.jeanhwea.leetcode.probset.ch04;
+
+import java.util.*;
+
+/**
+ * 逆波兰表达式求值
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-12, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution150 {
+
+  public static int evalRPN(String[] tokens) {
+    int ans = 0;
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    String[] tokens = {"2", "3", "+", "3", "*"};
+    System.out.println(evalRPN(tokens));
+  }
+}
