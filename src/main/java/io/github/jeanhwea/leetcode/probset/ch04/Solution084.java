2b86dc362b2a10ce2a55adfc6402bd93ecec6bb1
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-12 16:36:42 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch04/Solution084.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch04/Solution084.java
@@ -13,10 +13,28 @@ public class Solution084 {
 
   public static int largestRectangleArea(int[] a) {
     int n = a.length;
+
+    Deque<int[]> stack = new LinkedList<>();
+    stack.push(new int[] {-1, 0});
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+      while (!stack.isEmpty() && stack.peek()[1] > a[i]) {
+        stack.pop();
+      }
+      stack.push(new int[] {i, a[i]});
+
+      for (int[] e : stack) {
+        int area = (i - e[0] + 1) * e[1];
+        ans = Math.max(ans, area);
+      }
+    }
+
+    return ans;
   }
 
   public static void main(String[] args) {
-    int[] heights = {2, 1, 5, 6, 2, 3};
+    int[] heights = {2, 1, 6, 6, 2, 3};
     System.out.println(largestRectangleArea(heights));
   }
 }
