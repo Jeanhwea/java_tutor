1002857b35b032e6c7a0f462fb48d31371cf267f
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-13 20:05:13 +0800

    add: 恢复二叉搜索树

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch05/Solution099.java
@@ -0,0 +1,22 @@
+package io.github.jeanhwea.leetcode.probset.ch05;
+
+import java.util.*;
+
+/**
+ * 恢复二叉搜索树
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-13, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution099 {
+
+  public void recoverTree(TreeNode root) {
+    //
+  }
+
+  public static void main(String[] args) {
+    TreeNode tree01 = TreeNode.makeTree(new int[] {1, -1, 3, -1, -1, -1, 2});
+    TreeNode.dispTree(tree01);
+  }
+}
