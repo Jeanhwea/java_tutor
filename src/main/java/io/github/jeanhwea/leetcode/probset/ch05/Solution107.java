6455585167cb9d2b113481b4f51a948e815234b0
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-13 19:47:26 +0800

    add: 添加二叉树遍历

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch05/Solution107.java
@@ -0,0 +1,41 @@
+package io.github.jeanhwea.leetcode.probset.ch05;
+
+import java.util.*;
+
+/**
+ * 二叉树的层序遍历 II
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-13, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution107 {
+
+  public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+    List<List<Integer>> ans = new LinkedList<>();
+    if (root == null) return ans;
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      List<Integer> values = new LinkedList<>();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+        values.add(node.val);
+        if (node.left != null) queue.offer(node.left);
+        if (node.right != null) queue.offer(node.right);
+      }
+      ans.add(0, values);
+    }
+
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    TreeNode tree01 = TreeNode.makeTree(new int[] {1, 2, 3, 4, 5});
+    TreeNode.dispTree(tree01);
+    List<List<Integer>> res = levelOrderBottom(tree01);
+    System.out.println(res);
+  }
+}
---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch05/Solution145.java
@@ -0,0 +1,44 @@
+package io.github.jeanhwea.leetcode.probset.ch05;
+
+import java.util.*;
+
+/**
+ * 二叉树的后序遍历
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-13, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution145 {
+
+  public static List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> ans = new LinkedList<>();
+    if (root == null) return ans;
+
+    Deque<TreeNode> stack = new LinkedList<>();
+    TreeNode prev = null;
+    while (root != null || !stack.isEmpty()) {
+      while (root != null) {
+        stack.push(root);
+        root = root.left;
+      }
+      root = stack.peek();
+      if (root.right == null || root.right == prev) {
+        ans.add(root.val);
+        prev = stack.pop();
+        root = null;
+      } else {
+        root = root.right;
+      }
+    }
+
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    TreeNode tree01 = TreeNode.makeTree(new int[] {1, 2, 3, 4, 5});
+    TreeNode.dispTree(tree01);
+    List<Integer> res = postorderTraversal(tree01);
+    System.out.println(res);
+  }
+}
