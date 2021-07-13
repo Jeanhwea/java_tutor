34d411cf19a01f78d21a2e0b14d40bcd439aa49f
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-13 19:23:50 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch05/Solution094.java
@@ -0,0 +1,38 @@
+package io.github.jeanhwea.leetcode.probset.ch05;
+
+import java.util.*;
+
+/**
+ * 二叉树的中序遍历
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-13, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution094 {
+
+  public static List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> ans = new LinkedList<>();
+    if (root == null) return ans;
+
+    Deque<TreeNode> stack = new LinkedList<>();
+    while (root != null || !stack.isEmpty()) {
+      while (root != null) {
+        stack.push(root);
+        root = root.left;
+      }
+      root = stack.pop();
+      ans.add(root.val);
+      root = root.right;
+    }
+
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    TreeNode tree01 = TreeNode.makeTree(new int[] {1, 2, 3, 4, 5});
+    TreeNode.dispTree(tree01);
+    List<Integer> res = inorderTraversal(tree01);
+    System.out.println(res);
+  }
+}
