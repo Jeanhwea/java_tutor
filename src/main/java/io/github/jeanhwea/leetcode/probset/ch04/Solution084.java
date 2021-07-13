32aa25b38295bcd614f6d403fef9f721fb1eda4c
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-13 16:22:25 +0800

    add: 柱状图中最大的矩形

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch04/Solution084.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch04/Solution084.java
@@ -11,23 +11,35 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Solution084 {
 
+  // 单调栈
   public static int largestRectangleArea(int[] a) {
     int n = a.length;
+    int[] left = new int[n];
+    int[] right = new int[n];
 
-    Deque<int[]> stack = new LinkedList<>();
-    stack.push(new int[] {-1, 0});
+    Deque<Integer> stack = new LinkedList<>();
 
     int ans = 0;
     for (int i = 0; i < n; i++) {
-      while (!stack.isEmpty() && stack.peek()[1] > a[i]) {
+      while (!stack.isEmpty() && a[stack.peek()] >= a[i]) {
         stack.pop();
       }
-      stack.push(new int[] {i, a[i]});
+      left[i] = stack.isEmpty() ? -1 : stack.peek();
+      stack.push(i);
+    }
 
-      for (int[] e : stack) {
-        int area = (i - e[0] + 1) * e[1];
-        ans = Math.max(ans, area);
+    for (int i = n - 1; i >= 0; i--) {
+      while (!stack.isEmpty() && a[stack.peek()] >= a[i]) {
+        stack.pop();
       }
+      right[i] = stack.isEmpty() ? n : stack.peek();
+      stack.push(i);
+    }
+    System.out.println(Arrays.toString(left));
+    System.out.println(Arrays.toString(right));
+
+    for (int i = 0; i < n; i++) {
+      ans = Math.max(ans, (right[i] - left[i] - 1) * a[i]);
     }
 
     return ans;
