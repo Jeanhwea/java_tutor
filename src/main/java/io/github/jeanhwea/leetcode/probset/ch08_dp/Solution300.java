8bb2dc884f5e1b3fe908529d452515a67e384a22
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-16 22:12:30 +0800

    add: 添加 LIS

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution300.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution300.java
@@ -12,7 +12,22 @@ import java.util.*;
 public class Solution300 {
 
   public static int lengthOfLIS(int[] nums) {
-    return 0;
+    int n = nums.length;
+    // dp[i] 表示 nums[0..i-1] 的 LIS
+    int[] dp = new int[n + 1];
+    int maxIndex = 0;
+    Arrays.fill(dp, 1);
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j < i; j++) {
+        if (nums[i - 1] > nums[j - 1] && dp[j] + 1 > dp[i]) {
+          dp[i] = dp[j] + 1;
+        }
+      }
+      if (dp[i] > dp[maxIndex]) maxIndex = i;
+    }
+
+    return dp[maxIndex];
   }
 
   public static void main(String[] args) {
