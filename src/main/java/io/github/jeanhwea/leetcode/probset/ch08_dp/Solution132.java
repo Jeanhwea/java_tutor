4af95495e133101e2b0139326bdf9de04c705c74
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-20 14:19:42 +0800

    add: 添加检查最小回文串的分割次数

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution132.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution132.java
@@ -11,31 +11,45 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Solution132 {
 
+  // 两次动规求解
   public static int minCut(String str) {
     char[] a = str.toCharArray();
     int n = a.length;
 
     // dp[i-1][j-1] 表示 str 第 i 个字符到第 j 个字符是否为回文串
     boolean[][] dp = new boolean[n][n];
-    for (int i = 0; i < n; i++) dp[i][i] = true;
+    for (int i = 0; i < n; i++) Arrays.fill(dp[i], true);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = n - 1; i >= 0; i--) {
       for (int j = i + 1; j < n; j++) {
-        dp[i][j] = a[i] == a[j];
-        if (j - i > 1) {
-          dp[i][j] &= dp[i + 1][j - 1];
+        dp[i][j] = (a[i] == a[j]) && dp[i + 1][j - 1];
+      }
+    }
+
+    // System.out.println(Arrays.deepToString(dp).replaceAll("],", "],\n"));
+
+    // f[i] 表示前缀 str[0..i] 的分割次数
+    int[] f = new int[n];
+    Arrays.fill(f, Integer.MAX_VALUE);
+    for (int i = 0; i < n; i++) {
+      if (dp[0][i]) {
+        f[i] = 0;
+      } else {
+        for (int j = 0; j < i; j++) {
+          if (dp[j + 1][i]) {
+            f[i] = Math.min(f[i], f[j] + 1);
+          }
         }
       }
     }
 
-    // System.out.println(Arrays.deepToString(dp));
-
-    int ans = 0;
-    return ans;
+    return f[n - 1];
   }
 
   public static void main(String[] args) {
-    String str1 = "aab";
+    // String str1 = "abbab";
+    // String str1 = "abcccb";
+    String str1 = "cabababcbc";
     System.out.println(minCut(str1));
   }
 }
