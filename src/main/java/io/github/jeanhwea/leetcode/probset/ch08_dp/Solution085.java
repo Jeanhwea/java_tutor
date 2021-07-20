bdcbf659aaecaa49cae9ac70c533245a227bd141
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-20 16:45:11 +0800

    add: 添加求矩阵的最大面积

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution085.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution085.java
@@ -12,7 +12,33 @@ import java.util.*;
 public class Solution085 {
 
   public static int maximalRectangle(char[][] matrix) {
+    int m = matrix.length;
+    if (m == 0) return 0;
+    int n = matrix[0].length;
+    int[][] left = new int[m][n];
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (matrix[i][j] == '1') {
+          left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+        }
+      }
+    }
+    // System.out.println(Arrays.deepToString(left).replaceAll("],", "],\n"));
+
     int ans = 0;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (matrix[i][j] == '0') continue;
+        int width = left[i][j];
+        int area = width;
+        for (int k = i - 1; k >= 0; k--) {
+          width = Math.min(width, left[k][j]);
+          area = Math.max(area, (i - k + 1) * width);
+        }
+        ans = Math.max(ans, area);
+      }
+    }
     return ans;
   }
 
