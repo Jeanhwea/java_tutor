639510a98c6407a4df6a9698a85cd29b5c9d91b6
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-20 18:18:33 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution064.java
@@ -0,0 +1,24 @@
+package io.github.jeanhwea.leetcode.probset.ch08_dp;
+
+import java.util.*;
+
+/**
+ * 最小路径和
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-20, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution064 {
+
+  public static int minPathSum(int[][] grid) {
+    int n = grid.length, m = grid[0].length;
+
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    int[][] grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+    System.out.println(minPathSum(grid));
+  }
+}
