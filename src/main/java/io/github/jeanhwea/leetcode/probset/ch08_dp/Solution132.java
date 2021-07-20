8b38c1a94348fd761956fb84c48d2838fa726338
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-20 13:55:48 +0800

    add: 回文串 II

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution132.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution132.java
@@ -12,6 +12,24 @@ import java.util.*;
 public class Solution132 {
 
   public static int minCut(String str) {
+    char[] a = str.toCharArray();
+    int n = a.length;
+
+    // dp[i-1][j-1] 表示 str 第 i 个字符到第 j 个字符是否为回文串
+    boolean[][] dp = new boolean[n][n];
+    for (int i = 0; i < n; i++) dp[i][i] = true;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        dp[i][j] = a[i] == a[j];
+        if (j - i > 1) {
+          dp[i][j] &= dp[i + 1][j - 1];
+        }
+      }
+    }
+
+    // System.out.println(Arrays.deepToString(dp));
+
     int ans = 0;
     return ans;
   }
