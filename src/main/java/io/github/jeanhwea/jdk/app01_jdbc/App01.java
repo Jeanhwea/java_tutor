3c31ccd37fd3e116f3bec1c37260820121124d18
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-23 13:40:39 +0800

    add: 整理数据库的代码

--- src/main/java/io/github/jeanhwea/jdk/app01_jdbc/App01.java
+++ src/main/java/io/github/jeanhwea/jdk/app01_jdbc/App01.java
@@ -1,4 +1,34 @@
 package io.github.jeanhwea.jdk.app01_jdbc;
 
+import java.sql.*;
+
 public class App01 {
+
+  private static String url = "jdbc:mysql://localhost:3306/mysql?useSSL=false";
+  private static String username = "root";
+  private static String password = "root";
+
+  public static void test01() throws Exception {
+    // 加载驱动
+    Class.forName("com.mysql.jdbc.Driver");
+
+    // 创建连接
+    Connection conn = DriverManager.getConnection(url, username, password);
+
+    // 发送 SQL 语句
+    Statement stmt = conn.createStatement();
+    ResultSet rs = stmt.executeQuery("SELECT 'Hello'");
+    while (rs.next()) {
+      // rs.getObject(1);
+      String message = rs.getString(1);
+      System.out.println(message);
+    }
+
+    // 关闭连接
+    conn.close();
+  }
+
+  public static void main(String[] args) throws Exception {
+    test01();
+  }
 }
