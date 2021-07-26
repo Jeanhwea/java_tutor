670fdf343d5962befae01f8bcc9b6778d9e80da2
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-26 09:52:35 +0800

    add: 字符串和数组

---
+++ src/main/java/io/github/jeanhwea/language/jdk/p01_string/App01.java
@@ -0,0 +1,17 @@
+package io.github.jeanhwea.language.jdk.p01_string;
+
+import java.util.*;
+
+/**
+ * 字符串
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-26, JDK1.8
+ */
+@SuppressWarnings("all")
+public class App01 {
+
+  public static void main(String[] args) {
+    String str1 = "hello world";
+  }
+}
---
+++ src/main/java/io/github/jeanhwea/language/jdk/p02_array/App02.java
@@ -0,0 +1,17 @@
+package io.github.jeanhwea.language.jdk.p02_array;
+
+import java.util.*;
+
+/**
+ * 数组
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-26, JDK1.8
+ */
+@SuppressWarnings("all")
+public class App02 {
+
+  public static void main(String[] args) {
+    int[] arr1 = {1, 2, 3, 4};
+  }
+}
