72c6f5da186386d943f30d4d4070489bf639561a
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-27 08:58:05 +0800

    add: 添加 Java 反射

---
+++ src/main/java/io/github/jeanhwea/language/jvm/p01_reflection/App01.java
@@ -0,0 +1,50 @@
+package io.github.jeanhwea.language.jvm.p01_reflection;
+
+import java.lang.reflect.*;
+import java.util.*;
+
+/**
+ * 反射
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-27, JDK1.8
+ */
+@SuppressWarnings("all")
+public class App01 {
+
+  public static class Person {
+    private String name;
+
+    public Person() {}
+
+    public String getName() {
+      return this.name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    // 获取 Class 类
+    Class<?> clz = Class.forName("io.github.jeanhwea.language.jvm.p01_reflection.App01$Person");
+
+    // 获取类的构造器
+    Constructor<?> ctor = clz.getConstructor();
+    // 新建对象
+    Object obj = ctor.newInstance();
+
+    // 获取定义的字段
+    Field field = clz.getDeclaredField("name");
+    field.setAccessible(true); // private 修饰的字段需要配置可见性
+    // 设置字段值
+    field.set(obj, "Injected Person Name");
+
+    // 获取类的方法
+    Method method = clz.getMethod("getName");
+    // 调用方法
+    String name = (String) method.invoke(obj);
+    System.out.println(name);
+  }
+}
