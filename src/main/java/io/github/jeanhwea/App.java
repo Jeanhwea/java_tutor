00e23f866ed398a752c698e1c0157a2812675b1e
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-27 09:37:01 +0800

    add: 添加类加载器

--- src/main/java/io/github/jeanhwea/App.java
+++ src/main/java/io/github/jeanhwea/App.java
@@ -3,12 +3,7 @@ package io.github.jeanhwea;
 import java.util.*;
 
 public class App {
-
   public static void main(String[] args) {
-    ClassLoader loader = ClassLoader.getSystemClassLoader();
-    System.out.println(loader); // => sun.misc.Launcher$AppClassLoader@73d16e93
-    System.out.println(loader.getParent()); // => sun.misc.Launcher$ExtClassLoader@15db9742
-    System.out.println(loader.getParent().getParent()); // => null
     System.out.println("Hello World!");
   }
 }
---
+++ src/main/java/io/github/jeanhwea/language/jvm/p02_class_loader/App02.java
@@ -0,0 +1,20 @@
+package io.github.jeanhwea.language.jvm.p02_class_loader;
+
+import java.util.*;
+
+/**
+ * 添加类加载器
+ *
+ * @author Jinghui Hu
+ * @since 2021-07-27, JDK1.8
+ */
+@SuppressWarnings("all")
+public class App02 {
+
+  public static void main(String[] args) {
+    ClassLoader loader = ClassLoader.getSystemClassLoader();
+    System.out.println(loader); // => sun.misc.Launcher$AppClassLoader@73d16e93
+    System.out.println(loader.getParent()); // => sun.misc.Launcher$ExtClassLoader@15db9742
+    System.out.println(loader.getParent().getParent()); // => null
+  }
+}
