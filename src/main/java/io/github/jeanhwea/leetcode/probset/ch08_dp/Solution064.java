9bb04009108b41912fd243c18736e3d53ef41d24
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-02 13:30:13 +0800

    add: 最小路径和

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution064.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution064.java
@@ -12,9 +12,28 @@ import java.util.*;
 public class Solution064 {
 
   public static int minPathSum(int[][] grid) {
+    if (grid.length == 0 || grid[0].length == 0) return 0;
     int n = grid.length, m = grid[0].length;
-
-    return 0;
+    // dp[i][j] 表示当前落在 grid[i][j] 时所需要的最小路径和
+    int[][] dp = new int[n][m];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (i <= 0) {
+          if (j <= 0) {
+            dp[i][j] = grid[i][j];
+          } else {
+            dp[i][j] = dp[i][j - 1] + grid[i][j];
+          }
+        } else {
+          if (j <= 0) {
+            dp[i][j] = dp[i - 1][j] + grid[i][j];
+          } else {
+            dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+          }
+        }
+      }
+    }
+    return dp[n - 1][m - 1];
   }
 
   public static void main(String[] args) {
