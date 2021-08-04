4b42f0e5ffc567f7a19eeaa39570effe46e6a797
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-04 12:37:20 +0800

    add

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution140.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch08_dp/Solution140.java
@@ -11,19 +11,46 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Solution140 {
 
+  // 动态规划
   public static List<String> wordBreak(String s, List<String> wordDict) {
     int n = s.length();
-    int[] dp = new int[n + 1];
+    // 记录第 i 个字符结尾的字串包含的分割句子
+    Map<Integer, List<List<String>>> cache = new HashMap<>();
+
     for (int i = 1; i <= n; i++) {
-      for (int j = 1; j < i; j++) {
-        if (wordDict.contains(s.substring(1, i + 1))) {
-          dp[i] += dp[j];
+      for (int j = 0; j < i; j++) {
+        String currWord = s.substring(j, i);
+        if ((j == 0 || cache.containsKey(j)) && wordDict.contains(currWord)) {
+          List<List<String>> prevList = cache.getOrDefault(j, Collections.emptyList());
+          List<List<String>> currList = cache.getOrDefault(i, new LinkedList<>());
+          if (prevList.isEmpty()) {
+            List<String> newList = new LinkedList<String>();
+            newList.add(currWord);
+            currList.add(newList);
+          } else {
+            for (List<String> list : prevList) {
+              List<String> newList = new LinkedList<String>(list);
+              newList.add(currWord);
+              currList.add(newList);
+            }
+          }
+          cache.putIfAbsent(i, currList);
         }
       }
     }
+
+    // System.out.println(cache);
+    List<String> ans = new LinkedList<>();
+    for (List<String> strings : cache.getOrDefault(n, new LinkedList<>())) {
+      ans.add(String.join(" ", strings));
+    }
+
+    return ans;
   }
 
   public static void main(String[] args) {
-    // pass
+    String s = "catsanddog";
+    List<String> wordDict = Arrays.asList("cat", "cats", "and", "sand", "dog");
+    System.out.println(wordBreak(s, wordDict));
   }
 }
