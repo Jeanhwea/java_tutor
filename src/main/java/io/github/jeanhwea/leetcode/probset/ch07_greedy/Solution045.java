f88adf27f1c5221076525a0a5b0591670eae11f2
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-15 18:07:57 +0800

    add: 跳跃游戏 II

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch07_greedy/Solution045.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch07_greedy/Solution045.java
@@ -12,12 +12,20 @@ import java.util.*;
 public class Solution045 {
 
   public static int jump(int[] nums) {
-    return 0;
+    int n = nums.length, rightMost = 0, prevRightMost = 0, steps = 0;
+    for (int i = 0; i < n - 1; i++) {
+      rightMost = Math.max(rightMost, i + nums[i]);
+      if (i == prevRightMost) {
+        prevRightMost = rightMost;
+        steps++;
+      }
+    }
+    return steps;
   }
 
   public static void main(String[] args) {
     int[] a = new int[] {2, 3, 1, 1, 4};
     // int[] a = new int[] {3, 2, 0, 1, 4};
-    System.out.println(canJump(a));
+    System.out.println(jump(a));
   }
 }
