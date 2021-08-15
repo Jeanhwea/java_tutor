d6f2dc949d899d36664f4adb81e0d4a596e1a73a
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-15 10:11:23 +0800

    add: 添加有序链表构建二叉搜索树

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch05_tree/Solution109.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch05_tree/Solution109.java
@@ -1,7 +1,5 @@
 package io.github.jeanhwea.leetcode.probset.ch05_tree;
 
-import io.github.jeanhwea.leetcode.probset.ch02_list.ListNode;
-
 import java.util.*;
 
 /**
@@ -14,8 +12,23 @@ import java.util.*;
 public class Solution109 {
 
   public TreeNode sortedListToBST(ListNode head) {
-    //
-    return null;
+    return createTree(head, null);
+  }
+
+  // 构造 [beg, end) 区间的二叉搜索树
+  private TreeNode createTree(ListNode beg, ListNode end) {
+    if (beg == end) return null;
+
+    ListNode slow = beg, fast = beg;
+    while (fast != end && fast.next != end) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    TreeNode root = new TreeNode(slow.val);
+    root.left = createTree(beg, slow);
+    root.right = createTree(slow.next, end);
+    return root;
   }
 
   public static void main(String[] args) {
