4ab4ea08ab6ad3b6a1e225c7aa8cdcce0a09f2c7
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-15 21:59:22 +0800

    add: 盛最多水的容器

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch07_greedy/Solution011.java
@@ -0,0 +1,23 @@
+package io.github.jeanhwea.leetcode.probset.ch07_greedy;
+
+import java.util.*;
+
+/**
+ * 盛最多水的容器
+ *
+ * @author Jinghui Hu
+ * @since 2021-08-15, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution011 {
+
+  public static int maxArea(int[] height) {
+    int ans = 0;
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    int[] a = new int[] {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    System.out.println(maxArea(a));
+  }
+}
