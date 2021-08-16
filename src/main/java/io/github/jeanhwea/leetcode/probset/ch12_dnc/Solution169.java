9cd9a54116bfdac6f4d107d2c365201055bc24b0
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-16 11:36:23 +0800

    add: 添加分治法解决 majorityElement

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch12_dnc/Solution169.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch12_dnc/Solution169.java
@@ -11,10 +11,11 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Solution169 {
 
+  ////////////////////////////////////////////////////////////////////////////////
   // 选举法
-  public static int majorityElement(int[] a) {
+  public static int majorityElement1(int[] a) {
     int n = a.length, candiate = 0, count = 0;
-    // Step1: 选举, 选出备选人
+    // Step1: 预选, 选出候选人
     for (int i = 0; i < n; i++) {
       if (count <= 0) {
         candiate = a[i];
@@ -32,7 +33,37 @@ public class Solution169 {
     return -1;
   }
 
-  // 统计法
+  ////////////////////////////////////////////////////////////////////////////////
+  // 分治法
+  public static int majorityElement(int[] a) {
+    return merge(a, 0, a.length - 1);
+  }
+
+  // 合并 [beg, end] 区间的多数元素
+  private static int merge(int[] a, int beg, int end) {
+    if (beg == end) return a[beg];
+
+    int mid = beg + (end - beg) / 2;
+    int left = merge(a, beg, mid);
+    int right = merge(a, mid + 1, end);
+
+    if (left == right) return left;
+    int countLeft = countInRange(a, beg, mid, left);
+    int countRight = countInRange(a, mid + 1, end, right);
+    return countLeft > countRight ? left : right;
+  }
+
+  // 计算 [beg, end] 区间中 target 出现的次数
+  private static int countInRange(int[] a, int beg, int end, int target) {
+    int count = 0;
+    for (int i = beg; i <= end; i++) {
+      if (a[i] == target) count++;
+    }
+    return count;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // 统计法, 使用哈希表统计
   public static int majorityElement0(int[] a) {
     int n = a.length;
     Map<Integer, Integer> count = new HashMap<>();
