cfed202629d3179d9557c0cd38bffd1bf962e737
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-16 11:06:04 +0800

    add: 添加合并 K 个升序链表

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch09_sort_search/Solution023.java
@@ -0,0 +1,21 @@
+package io.github.jeanhwea.leetcode.probset.ch09_sort_search;
+
+import java.util.*;
+
+/**
+ * 合并 K 个升序链表
+ *
+ * @author Jinghui Hu
+ * @since 2021-08-16, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution023 {
+
+  public static ListNode mergeKLists(ListNode[] lists) {
+    return null;
+  }
+
+  public static void main(String[] args) {
+    // pass
+  }
+}
