f3d258c6596f8cb0d5cafa5b54f8964aed12e94f
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-16 12:47:28 +0800

    add: 合并 K 个有序链表

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch09_sort_search/Solution023.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch09_sort_search/Solution023.java
@@ -12,10 +12,48 @@ import java.util.*;
 public class Solution023 {
 
   public static ListNode mergeKLists(ListNode[] lists) {
-    return null;
+    return mergeList(lists, 0, lists.length);
+  }
+
+  private static ListNode mergeList(ListNode[] lists, int beg, int end) {
+    // 边界条件
+    if (beg > lists.length - 1) return null;
+    if (beg >= end) return lists[beg];
+
+    // 分治法, 将数组分成两个部分处理
+    int mid = beg + (end - beg) / 2;
+    ListNode p = mergeList(lists, beg, mid);
+    ListNode q = mergeList(lists, mid + 1, end);
+
+    // 合并链表
+    ListNode dummy = new ListNode(-1), t = dummy;
+    while (p != null && q != null) {
+      if (p.val < q.val) {
+        t.next = p;
+        p = p.next;
+        t = t.next;
+        t.next = null;
+      } else {
+        t.next = q;
+        q = q.next;
+        t = t.next;
+        t.next = null;
+      }
+    }
+    t.next = p != null ? p : q;
+    return dummy.next;
   }
 
   public static void main(String[] args) {
-    // pass
+    List<ListNode> lists = new LinkedList<>();
+    lists.add(ListNode.makeList(new int[] {1, 4, 5}));
+    lists.add(ListNode.makeList(new int[] {1, 3, 4}));
+    lists.add(ListNode.makeList(new int[] {2, 6}));
+    ListNode[] arr = new ListNode[lists.size()];
+    for (int i = 0; i < lists.size(); i++) {
+      arr[i] = lists.get(i);
+    }
+    ListNode res = mergeKLists(arr);
+    ListNode.dispList(res);
   }
 }
