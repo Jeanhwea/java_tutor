c986f9130cfb72f642e36cd5181d1d459c103f2a
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-17 14:33:44 +0800

    add: Range 模块

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch09_sort_search/Solution715.java
@@ -0,0 +1,37 @@
+package io.github.jeanhwea.leetcode.probset.ch09_sort_search;
+
+import java.util.*;
+
+/**
+ * Range 模块
+ *
+ * @author Jinghui Hu
+ * @since 2021-08-17, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution715 {
+
+  static class RangeModule {
+
+    public RangeModule() {
+      // pass
+    }
+
+    public void addRange(int left, int right) {
+      // pass
+    }
+
+    public boolean queryRange(int left, int right) {
+      // pass
+      return true;
+    }
+
+    public void removeRange(int left, int right) {
+      // pass
+    }
+  }
+
+  public static void main(String[] args) {
+    // pass
+  }
+}
