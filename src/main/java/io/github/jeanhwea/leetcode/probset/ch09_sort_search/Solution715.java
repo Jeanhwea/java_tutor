17b6db247b193412c8b35d4f3b43a2fad234f5d2
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-17 18:46:49 +0800

    add: Range 模块

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch09_sort_search/Solution715.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch09_sort_search/Solution715.java
@@ -12,22 +12,58 @@ import java.util.*;
 public class Solution715 {
 
   static class RangeModule {
+    private TreeSet<Interval> ranges;
 
     public RangeModule() {
-      // pass
+      ranges = new TreeSet<>();
     }
 
+    // 添加 [left, right) 区间
     public void addRange(int left, int right) {
-      // pass
+      Iterator<Interval> iter = ranges.tailSet(new Interval(0, left - 1)).iterator();
+      while (iter.hasNext()) {
+        Interval interval = iter.next();
+        if (right < interval.left) break;
+        left = Math.min(left, interval.left);
+        right = Math.max(right, interval.right);
+        iter.remove();
+      }
+      ranges.add(new Interval(left, right));
     }
 
+    // 查询是否包含 [left, right) 区间
     public boolean queryRange(int left, int right) {
-      // pass
-      return true;
+      Interval interval = ranges.higher(new Interval(0, left));
+      return (interval != null && interval.left <= left && right <= interval.right);
     }
 
+    // 移除 [left, right) 区间
     public void removeRange(int left, int right) {
-      // pass
+      Iterator<Interval> iter = ranges.tailSet(new Interval(0, left)).iterator();
+      ArrayList<Interval> todoList = new ArrayList();
+      while (iter.hasNext()) {
+        Interval interval = iter.next();
+        if (right < interval.left) break;
+        if (interval.left < left) todoList.add(new Interval(interval.left, left));
+        if (right < interval.right) todoList.add(new Interval(right, interval.right));
+        iter.remove();
+      }
+      for (Interval interval : todoList) ranges.add(interval);
+    }
+
+    class Interval implements Comparable<Interval> {
+      int left, right;
+
+      public Interval(int left, int right) {
+        this.left = left;
+        this.right = right;
+      }
+
+      @Override
+      public int compareTo(Interval that) {
+        if (this.right == that.right) return this.left - this.right;
+        return this.right - that.right;
+      }
     }
   }
 
