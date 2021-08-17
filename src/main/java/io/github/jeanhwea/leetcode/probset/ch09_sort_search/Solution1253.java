f11a10243f7ee755c77bf1bf4841ee67c0bd4f53
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-17 14:35:02 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch09_sort_search/Solution1253.java
@@ -0,0 +1,18 @@
+package io.github.jeanhwea.leetcode.probset.ch09_sort_search;
+
+import java.util.*;
+
+/**
+ * 重构 2 行二进制矩阵
+ *
+ * @author Jinghui Hu
+ * @since 2021-08-17, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution1253 {
+
+  public static void main(String[] args) {
+    // pass
+  }
+
+}
