faf0ab2dc07ee1d283a7f684fd787f42461921a7
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-24 08:21:14 +0800

    add: 电话的组合数字

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch10_backtrack/Solution017.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch10_backtrack/Solution017.java
@@ -11,11 +11,43 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Solution017 {
 
+  private static String[] button =
+      new String[] {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+  private static List<String> ans;
+  private static StringBuffer choose;
+  private static String a;
+  private static int n;
+
+  private static void backtrack(int k) {
+    if (k == n) {
+      ans.add(new String(choose));
+      return;
+    }
+
+    String letters = button[a.charAt(k) - '2'];
+    for (int i = 0; i < letters.length(); i++) {
+      choose.append(letters.charAt(i));
+      backtrack(k + 1);
+      choose.deleteCharAt(choose.length() - 1);
+    }
+  }
+
   public static List<String> letterCombinations(String digits) {
-    // pass
+    a = digits;
+    n = digits.length();
+    ans = new LinkedList<>();
+    choose = new StringBuffer();
+
+    if (digits.length() <= 0) return ans;
+
+    backtrack(0);
+    return ans;
   }
 
   public static void main(String[] args) {
-    // pass
+    // String digits = "23";
+    String digits = "";
+    System.out.println(letterCombinations(digits));
   }
 }
