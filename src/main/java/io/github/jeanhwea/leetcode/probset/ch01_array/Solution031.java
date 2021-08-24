0585cf1f8e52d34cb696afcc1736c9432221c290
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-24 08:56:01 +0800

    update: 整理下一个排列的算法

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch01_array/Solution031.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch01_array/Solution031.java
@@ -14,14 +14,19 @@ public class Solution031 {
   public static void nextPermutation(int[] a) {
     int n = a.length;
 
+    // Step1: 从右向左扫描得到递减的数值
     int i = n - 2;
     while (i >= 0 && a[i] >= a[i + 1]) i--;
+
+    // Step2: 如果找到递减值 a[i], 则从右向左扫描得到比 a[i] 大的值
     if (i >= 0) {
       int j = n - 1;
       while (j >= 0 && a[j] <= a[i]) j--;
+      // 找到后交换 a[i], a[j]
       if (j >= 0) swap(a, i, j);
     }
 
+    // Step3: 整体翻转列表 a[i+1:]
     reverse(a, i + 1, n - 1);
   }
 
