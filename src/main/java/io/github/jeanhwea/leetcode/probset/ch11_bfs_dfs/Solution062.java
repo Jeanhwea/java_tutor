61cb4e802d66eed12ba93374cf2dbfefe4da093b
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-28 23:34:54 +0800

    add

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch11_bfs_dfs/Solution062.java
@@ -0,0 +1,21 @@
+package io.github.jeanhwea.leetcode.probset.ch11_bfs_dfs;
+
+import java.util.*;
+
+/**
+ * 不同路径
+ *
+ * @author Jinghui Hu
+ * @since 2021-08-28, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution062 {
+
+  public static int uniquePaths(int m, int n) {
+    //
+  }
+
+  public static void main(String[] args) {
+    //
+  }
+}
---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch11_bfs_dfs/Solution063.java
@@ -0,0 +1,17 @@
+package io.github.jeanhwea.leetcode.probset.ch11_bfs_dfs;
+
+import java.util.*;
+
+/**
+ * 不同路径 II
+ *
+ * @author Jinghui Hu
+ * @since 2021-08-28, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution063 {
+
+  public static void main(String[] args) {
+  }
+
+}
