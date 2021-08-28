8b70e985c7c5f8b2794fe06f01177f12c1d4a1f1
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-28 23:05:32 +0800

    add: 分割回文串

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch11_bfs_dfs/Solution131.java
@@ -0,0 +1,21 @@
+package io.github.jeanhwea.leetcode.probset.ch11_bfs_dfs;
+
+import java.util.*;
+
+/**
+ * 分割回文串
+ *
+ * @author Jinghui Hu
+ * @since 2021-08-28, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution131 {
+
+  public static List<List<String>> partition(String s) {
+    //
+  }
+
+  public static void main(String[] args) {
+    String s = "aab";
+  }
+}
