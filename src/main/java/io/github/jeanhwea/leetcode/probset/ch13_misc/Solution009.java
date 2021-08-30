800d234c0a6ba4ac3e3251958bb90082b3e73fc3
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-30 18:37:22 +0800

    update

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch13_misc/Solution009.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch13_misc/Solution009.java
@@ -12,14 +12,16 @@ import java.util.*;
 public class Solution009 {
 
   public static boolean isPalindrome(int x) {
+    // 负数不可能是回文数
     if (x < 0) return false;
+    // 除 0 以外, 以 0 结尾的数字不可能是回文数, 因为最高位不可能为 0
     if (x % 10 == 0 && x != 0) return false;
-    int y = 0;
-    while (x > y) {
-      y = y * 10 + x % 10;
+    int rev = 0; // 记录高位
+    while (x > rev) {
+      rev = rev * 10 + x % 10;
       x /= 10;
     }
-    return x == y || x == y / 10;
+    return x == rev || x == rev / 10;
   }
 
   public static void main(String[] args) {
