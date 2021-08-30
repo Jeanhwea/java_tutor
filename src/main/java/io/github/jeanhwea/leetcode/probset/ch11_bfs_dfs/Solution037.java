9ae73bc2ef0032d26d91b382ae78b214171aa142
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-08-30 11:41:48 +0800

    add: 判断是否是合法的数独逻辑

---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch11_bfs_dfs/Solution037.java
@@ -0,0 +1,67 @@
+package io.github.jeanhwea.leetcode.probset.ch11_bfs_dfs;
+
+import java.util.*;
+
+/**
+ * 解数独
+ *
+ * @author Jinghui Hu
+ * @since 2021-08-30, JDK1.8
+ */
+@SuppressWarnings("all")
+public class Solution037 {
+
+  public static void solveSudoku(char[][] board) {
+    n = board.length;
+    board = a;
+  }
+
+  private static char[][] a;
+  private static int n;
+
+  private static void dfs(int x, int y) {
+    //
+  }
+
+  @SuppressWarnings("unchecked")
+  private static boolean isValidSudoku(char[][] board) {
+    Map<Character, Integer>[] mp1 = new HashMap[9], mp2 = new HashMap[9], mp3 = new HashMap[9];
+    for (int i = 0; i < 9; i++) {
+      mp1[i] = new HashMap<>();
+      mp2[i] = new HashMap<>();
+      mp3[i] = new HashMap<>();
+    }
+
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        char ch = board[i][j];
+        if (ch == '.') continue;
+        int k = (i / 3) * 3 + j / 3;
+
+        mp1[i].put(ch, mp1[i].getOrDefault(ch, 0) + 1);
+        mp2[j].put(ch, mp2[j].getOrDefault(ch, 0) + 1);
+        mp3[k].put(ch, mp3[k].getOrDefault(ch, 0) + 1);
+
+        if (mp1[i].get(ch) > 1 || mp2[j].get(ch) > 1 || mp3[k].get(ch) > 1) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    char[][] board = {
+      {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+      {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+      {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+      {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+      {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+      {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+      {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+      {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+      {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+    };
+    System.out.println(isValidSudoku(board));
+  }
+}
