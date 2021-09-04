1fcc18bf960957d6ca4deaa56738f7097ae5bd67
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-09-04 23:39:25 +0800

    add: 和为 s 的数字

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch13_misc/Solution043.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch13_misc/Solution043.java
@@ -11,12 +11,49 @@ import java.util.*;
 @SuppressWarnings("all")
 public class Solution043 {
 
-  public String multiply(String num1, String num2) {
+  public static String multiply(String num1, String num2) {
+    int n = num1.length(), m = num2.length();
+    // 特殊情况
+    if ("0".equals(num1) || "0".equals(num2)) return "0";
+    // 计算过程
     String ans = "";
+    for (int k = m - 1; k >= 0; k--) {
+      StringBuffer res = new StringBuffer();
+      int carry = 0;
+      for (int j = 1; j < m - k; j++) res.append('0');
+      int d = num2.charAt(k) - '0';
+      for (int i = n - 1; i >= 0; i--) {
+        int a = num1.charAt(i) - '0';
+        int product = a * d + carry;
+        res.append((char) (product % 10 + '0'));
+        carry = product / 10;
+      }
+      if (carry > 0) res.append((char) (carry + '0'));
+      ans = add(ans, res.reverse().toString());
+    }
     return ans;
+  }
+
+  private static String add(String num1, String num2) {
+    StringBuffer ans = new StringBuffer();
+    int carry = 0, n = num1.length(), m = num2.length();
+    int i = n - 1, j = m - 1;
+    while (i >= 0 || j >= 0) {
+      int n1 = i >= 0 ? num1.charAt(i--) - '0' : 0;
+      int n2 = j >= 0 ? num2.charAt(j--) - '0' : 0;
+      int sum = n1 + n2 + carry;
+      // System.out.printf("n1=%d,n2=%d,sum=%d\n", n1, n2, sum);
+      ans.append((char) ((sum % 10) + '0'));
+      carry = sum / 10;
+    }
+    if (carry > 0) ans.append((char) (carry + '0'));
+    return ans.reverse().toString();
   }
 
   public static void main(String[] args) {
     System.out.println(multiply("123", "456"));
+    System.out.println(multiply("0", "0"));
+    System.out.println(multiply("9", "9"));
+    // System.out.println(add("923", "459"));
   }
 }
---
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch13_misc/SolutionOF057.java
@@ -0,0 +1,50 @@
+package io.github.jeanhwea.leetcode.probset.ch13_misc;
+
+import java.util.*;
+
+/**
+ * 和为 s 的两个数字
+ *
+ * @author Jinghui Hu
+ * @since 2021-09-04, JDK1.8
+ */
+@SuppressWarnings("all")
+public class SolutionOf057 {
+
+  public static int[] twoSum(int[] nums, int target) {
+    int i = 0, j = nums.length - 1;
+    while (i < j) {
+      if (nums[i] + nums[j] == target) {
+        return new int[] {nums[i], nums[j]};
+      } else if (nums[i] + nums[j] < target) {
+        i++; // 最小的加最大的都比 target 小，舍弃最小的
+      } else {
+        j--; // 最大的加最小的都比 target 大，舍弃最大的
+      }
+    }
+    return new int[0];
+  }
+
+  public static int[] twoSum0(int[] nums, int target) {
+    int n = nums.length;
+    for (int i = 0; i < n; i++) {
+      int goal = target - nums[i];
+      int beg = i + 1, end = n - 1;
+      while (beg <= end) {
+        int mid = beg + (end - beg) / 2;
+        if (nums[mid] == goal) {
+          return new int[] {nums[i], nums[mid]};
+        } else if (nums[mid] < goal) {
+          beg = mid + 1;
+        } else {
+          end = mid - 1;
+        }
+      }
+    }
+    return null;
+  }
+
+  public static void main(String[] args) {
+    // pass
+  }
+}
