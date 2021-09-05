1edf7c19db587a314b6b9f5795434143a0163476
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-09-05 21:43:05 +0800

    add: 解数独

--- src/main/java/io/github/jeanhwea/leetcode/probset/ch11_bfs_dfs/Solution037.java
+++ src/main/java/io/github/jeanhwea/leetcode/probset/ch11_bfs_dfs/Solution037.java
@@ -12,76 +12,37 @@ import java.util.*;
 public class Solution037 {
 
   public static void solveSudoku(char[][] board) {
-    n = board.length;
-    choose = board;
-    seen = new boolean[n][n];
-    ans = false;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        seen[i][j] = '.' == board[i][j];
-      }
-    }
-    dfs(0, 0);
+    dfs(board);
   }
 
-  private static boolean[][] seen;
-  private static char[][] choose;
-  private static int n;
-  private static boolean ans;
-
-  private static void dfs(int x, int y) {
-    if (x == n && y == n) {
-      ans = true;
-      System.out.println(Arrays.deepToString(choose));
-      return;
-    }
-    // System.out.printf("x=%d, y=%d\n", x, y);
-
-    if (x < 0 || x >= n || y < 0 || y >= n || !isValidSudoku(choose)) return;
-
-    for (char ch = '1'; ch <= '9'; ch++) {
-      if (seen[x][y]) {
-        if (x < n - 1) {
-          dfs(x + 1, y);
-        } else {
-          dfs(0, y + 1);
-        }
-      } else {
-        choose[x][y] = ch;
-        seen[x][y] = true;
-        if (x < n - 1) {
-          dfs(x + 1, y);
-        } else {
-          dfs(0, y + 1);
-        }
-        seen[x][y] = false;
-        choose[x][y] = '.';
-      }
-    }
-  }
-
-  @SuppressWarnings("unchecked")
-  private static boolean isValidSudoku(char[][] board) {
-    Map<Character, Integer>[] mp1 = new HashMap[9], mp2 = new HashMap[9], mp3 = new HashMap[9];
-    for (int i = 0; i < 9; i++) {
-      mp1[i] = new HashMap<>();
-      mp2[i] = new HashMap<>();
-      mp3[i] = new HashMap<>();
-    }
-
+  // 回溯法搜索解
+  private static boolean dfs(char[][] board) {
     for (int i = 0; i < 9; i++) {
       for (int j = 0; j < 9; j++) {
-        char ch = board[i][j];
-        if (ch == '.') continue;
-        int k = (i / 3) * 3 + j / 3;
-
-        mp1[i].put(ch, mp1[i].getOrDefault(ch, 0) + 1);
-        mp2[j].put(ch, mp2[j].getOrDefault(ch, 0) + 1);
-        mp3[k].put(ch, mp3[k].getOrDefault(ch, 0) + 1);
-
-        if (mp1[i].get(ch) > 1 || mp2[j].get(ch) > 1 || mp3[k].get(ch) > 1) {
+        if (board[i][j] == '.') {
+          for (int k = 0; k < 9; k++) {
+            board[i][j] = (char) ('1' + k);
+            if (isValid(board, i, j) && dfs(board)) return true;
+            board[i][j] = '.';
+          }
           return false;
         }
+      }
+    }
+    return true;
+  }
+
+  // 检查位置 (x,y) 是否合法
+  private static boolean isValid(char[][] board, int x, int y) {
+    for (int i = 0; i < 9; i++) {
+      if (i != x && board[i][y] == board[x][y]) return false;
+    }
+    for (int j = 0; j < 9; j++) {
+      if (j != y && board[x][j] == board[x][y]) return false;
+    }
+    for (int i = 3 * (x / 3); i < 3 * (x / 3 + 1); i++) {
+      for (int j = 3 * (y / 3); j < 3 * (y / 3 + 1); j++) {
+        if ((i != x || j != y) && board[i][j] == board[x][y]) return false;
       }
     }
     return true;
@@ -100,5 +61,6 @@ public class Solution037 {
       {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
     };
     solveSudoku(board);
+    System.out.println(Arrays.deepToString(board));
   }
 }
