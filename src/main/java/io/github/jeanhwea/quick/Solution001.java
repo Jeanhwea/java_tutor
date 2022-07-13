e48f419baf0dacbc2ec5b21f5d7ddd12a61f7aad
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-07-13 11:24:01 +0800

    update: add quick scripts

---
+++ src/main/java/io/github/jeanhwea/quick/Solution001.java
@@ -0,0 +1,12 @@
+package io.github.jeanhwea.quick;
+
+import java.util.*;
+
+public class Solution001 {
+
+  public static void main(String[] args) {
+    List<Integer> arr = Arrays.asList(1, 2, 3);
+    System.out.println("Hello world");
+    System.out.println(arr);
+  }
+}
